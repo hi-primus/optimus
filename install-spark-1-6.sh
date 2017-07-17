45287142941faa71affcb17e3bcb0d800243bc4d
Favio André Vázquez <favio.vazquezp@gmail.com>
2017-07-17 12:27:35 -0500

    Update spark version

--- install-spark-1-6.sh
+++ install-spark-1-6.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-SPARK_VERSION='1.6.0'
+SPARK_VERSION='1.6.3'
 HADOOP_VERSION='2.6'
 
 curl http://apache.mirror.gtcomm.net/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz --output /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
