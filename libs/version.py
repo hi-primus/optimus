e14932d8e665864ff963489c12bc1c1ec244c76f
Favio André Vázquez <favio.vazquezp@gmail.com>
2017-07-17 13:00:06 -0500

    Create version.py

---
+++ libs/version.py
@@ -0,0 +1,9 @@
+def _safe_int(string):
+    try:
+        return int(string)
+    except ValueError:
+        return string
+
+
+__version__ = '0.1.0'
+VERSION = tuple(_safe_int(x) for x in __version__.split('.'))
