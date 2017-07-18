97bd1332cf0561275ba4f3567e060385654ba353
faviovazquez <favio.vazquezp@gmail.com>
2017-07-18 15:39:14 -0500

    Rename main directory

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 # Get version without importing, which avoids dependency issues
 def get_version():
-    with open('libs/version.py') as version_file:
+    with open('optimus/version.py') as version_file:
         return re.search(r"""__version__\s+=\s+(['"])(?P<version>.+?)\1""",
                          version_file.read()).group('version')
                          
