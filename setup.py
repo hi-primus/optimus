cd6e6f655e9bd92945b77aa79264cf23de3170d5
faviovazquez <favio.vazquezp@gmail.com>
2017-07-18 13:03:36 -0500

    Pump up version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     author='Favio Vazquez',
     author_email='favio.vazquez@ironmussa.com',
     url='https://github.com/ironmussa/Optimus/',
-    download_url = 'https://github.com/ironmussa/Optimus/archive/0.1.0.tar.gz',
+    download_url = 'https://github.com/ironmussa/Optimus/archive/0.1.2.tar.gz',
     description=('Optimus is the missing library for cleaning and preprocessing data in a distributed fashion.'),
     long_description=readme(),
     license='APACHE',
