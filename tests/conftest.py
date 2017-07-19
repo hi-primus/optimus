564e9c3a6bd7808b052d40826c493d350f253772
Favio André Vázquez <favio.vazquezp@gmail.com>
2017-07-19 10:43:06 -0500

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,44 @@
+import logging
+import pytest
+
+from pyspark import HiveContext
+from pyspark import SparkConf
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+
+def quiet_py4j():
+    """ turn down spark logging for the test context """
+    logger = logging.getLogger('py4j')
+    logger.setLevel(logging.WARN)
+
+
+@pytest.fixture(scope="session")
+def spark_context(request):
+    """ fixture for creating a spark context
+    Args:
+        request: pytest.FixtureRequest object
+    """
+    conf = (SparkConf().setMaster("local[2]").setAppName("pytest-pyspark-local-testing"))
+    sc = SparkContext(conf=conf)
+    request.addfinalizer(lambda: sc.stop())
+
+    quiet_py4j()
+    return sc
+
+
+@pytest.fixture(scope="session")
+def hive_context(spark_context):
+    """  fixture for creating a Hive Context. Creating a fixture enables it to be reused across all
+        tests in a session
+    Args:
+        spark_context: spark_context fixture
+    Returns:
+        HiveContext for tests
+    """
+    return HiveContext(spark_context)
+
+
+@pytest.fixture(scope="session")
+def streaming_context(spark_context):
+    return StreamingContext(spark_context, 1)
