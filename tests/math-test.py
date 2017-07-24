bddbf66726e526ea4878a715f25f3826d393d855
Favio André Vázquez <favio.vazquezp@gmail.com>
2017-07-24 16:42:01 -0500

    Update math-test.py

--- tests/math-test.py
+++ tests/math-test.py
@@ -1,20 +1,4 @@
-def square(x):
-    """Return the square of `x`.
-    Parameters
-    ----------
-    x : int or float
-        The input number.
-    Returns
-    -------
-    x2 : same type as `x`
-        The square of the input value.
-    Examples
-    --------
-    >>> square(5)
-    25
-    """
-    return x ** 2
+def test_spark_context_fixture(spark_context):
+    test_rdd = spark_context.parallelize([1, 2, 3, 4])
 
-def test_square():
-    x = 4
-    assert square(x) == 16
+    assert test_rdd.count() == 4
