1cfbf1252a05d47d416f9ecd4b0bd436ad54313f
faviovazquez <favio.vazquezp@gmail.com>
2017-09-13 12:33:36 -0500

    Update testing for coverage

--- tests/tests.py
+++ tests/tests.py
@@ -2,11 +2,11 @@ import airbrake
 
 logger = airbrake.getLogger()
 
-try:
-    def test_spark_context_fixture(spark_context):
-        test_rdd = spark_context.parallelize([1, 2, 3, 4])
 
+def test_spark_context_fixture(spark_context):
+    test_rdd = spark_context.parallelize([1, 2, 3, 4])
+
+    try:
         assert test_rdd.count() == 4
-
-except Exception:
-    logger.exception("Bad math.")
+    except Exception:
+        logger.exception("Wrong count.")
