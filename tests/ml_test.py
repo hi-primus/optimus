7acbf492e2119405ea80f4d7ba537d5b96a9b1d6
faviovazquez <favio.vazquezp@gmail.com>
2017-11-07 13:18:54 -0600

    Fix ml tests

--- tests/ml_test.py
+++ tests/ml_test.py
@@ -4,17 +4,14 @@ from pyspark.ml import feature, classification
 from nose.tools import assert_equal
 
 
-class TestML(object):
+def test_ml_pipe():
+    df = op.sc. \
+        parallelize([Row(sentence='this is a test', label=0.),
+                     Row(sentence='this is another test', label=1.)]). \
+        toDF()
 
-    @classmethod
-    def test_ml_pipe(cls):
-        df = op.sc. \
-            parallelize([Row(sentence='this is a test', label=0.),
-                         Row(sentence='this is another test', label=1.)]). \
-            toDF()
+    pl = feature.Tokenizer().setInputCol('sentence') | feature.CountVectorizer()
+    ml = pl | classification.LogisticRegression()
 
-        pl = feature.Tokenizer().setInputCol('sentence') | feature.CountVectorizer()
-        ml = pl | classification.LogisticRegression()
-
-        ml_model = ml.fit(df)
-        assert_equal(ml_model.transform(df).count(), 2)
+    ml_model = ml.fit(df)
+    assert_equal(ml_model.transform(df).count(), 2)
