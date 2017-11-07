82e408c639be7e78d3f0c43ff06cfcb913fe13e1
faviovazquez <favio.vazquezp@gmail.com>
2017-11-07 13:25:10 -0600

    Add logistic_regression_text test

--- tests/ml_test.py
+++ tests/ml_test.py
@@ -2,6 +2,8 @@ import optimus as op
 from pyspark.sql import Row
 from pyspark.ml import feature, classification
 from nose.tools import assert_equal
+import pyspark
+
 
 
 def test_ml_pipe():
@@ -15,3 +17,16 @@ def test_ml_pipe():
 
     ml_model = ml.fit(df)
     assert_equal(ml_model.transform(df).count(), 2)
+
+
+def test_logistic_regression_text():
+    df = op.sc. \
+        parallelize([Row(sentence='this is a test', label=0.),
+                     Row(sentence='this is another test', label=1.)]). \
+        toDF()
+
+    df_predict, ml_model = op.ml.logistic_regression_text(df, "sentence")
+
+    assert isinstance(df_predict, pyspark.sql.dataframe.DataFrame),  "Error for df_predict"
+
+    assert isinstance(ml_model, pyspark.ml.PipelineModel), "Error for ml_model"
