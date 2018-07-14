1fe080d009a104f9d73ca6a0c19e2a7a592ee995
Argenis Leon <argenisleon@gmail.com>
2018-07-14 00:40:55 -0500

    Created helper funtions file

---
+++ optimus/helpers/functions.py
@@ -0,0 +1,10 @@
+from IPython.display import display, HTML
+
+
+def print_html(html):
+    """
+    Just a display() helper to print html code
+    :param html: html code to be printed
+    :return:
+    """
+    display(HTML(html))
