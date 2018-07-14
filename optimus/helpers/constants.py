19ede0e14d044346595f2335b4e7f907b81c687d
Argenis Leon <argenisleon@gmail.com>
2018-07-14 02:30:15 -0500

    var_types now get all the spark data types

--- optimus/helpers/constants.py
+++ optimus/helpers/constants.py
@@ -1,13 +1,20 @@
-from pyspark.sql.types import *
+from pyspark.sql import types as t
+
 
 # You can use string, str or String as param
-TYPES = {'string': 'string', 'str': 'string', 'String': 'string', 'integer': 'int',
-         'int': 'int', 'float': 'float', 'double': 'double', 'Double': 'double'}
+TYPES = {'string': 'string',
+         'str': 'string',
+         'String': 'string',
+         'integer': 'int',
+         'int': 'int',
+         'float': 'float',
+         'double': 'double',
+         'Double': 'double'}
 
 # Instead StringType() just use string
 DICT_TYPES = {'string': StringType(), 'int': IntegerType(), 'float': FloatType(), 'double': DoubleType()}
 
-VAR_TYPES = (StringType, IntegerType, FloatType, DoubleType)
+VAR_TYPES = t.__all__
 
 JUST_CHECKING = "Just checking that all necessary environments vars are present..."
 STARTING = "Starting or getting SparkSession and SparkContext..."
