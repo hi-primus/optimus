2a68888f8d28c132e381fb7093dd048632b845a1
Argenis Leon <argenisleon@gmail.com>
2018-07-15 19:07:25 -0500

    Working in a profiler

--- optimus/profiler/profiler.py
+++ optimus/profiler/profiler.py
@@ -1,26 +1,27 @@
 # Reference http://nbviewer.jupyter.org/github/julioasotodv/spark-df-profiling/blob/master/examples/Demo.ipynb
 
 import pyspark.sql.functions as F
+from optimus.helpers.functions import *
 
 
 class Profiler:
     def __init__(self, df):
         self._df = df
-        pass
 
-    def column(self):
+    def column(self, columns):
         df = self._df
+
+        columns = parse_columns(df, columns)
+
         print(len(df.columns))
         print(df.count())
 
-        print(df.select([F.count(F.when(F.isnan(c), c)).alias(c) for c in df.columns]).show())
+        print(df.cols().count_na(columns))
 
-        df.cols().count_na("*")
+        print(df.cols().count_zeros(columns))
 
-        df.cols().count_na("*")
+        print(df.cols().count_uniques(columns))
 
         print(df.dtypes)
-
-        print([df.distinct(c).count() for c in df.columns])
 
         return
