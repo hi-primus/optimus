974eeffdc54786c981069618ee40459b1db9fc8c
Argenis Leon <argenisleon@gmail.com>
2018-07-19 10:25:40 -0500

    Now we take a data sample to infer the column data type

--- optimus/profiler/profiler.py
+++ optimus/profiler/profiler.py
@@ -39,32 +39,10 @@ class Profiler:
 
         return col_type
 
-    def columns_by_types(self, columns):
+    def count_data_types(self, df, columns):
         """
-
-        :param columns:
-        :return:
-        """
-        columns = parse_columns(self._df, columns)
-
-        result = self.count_data_types(columns)
-
-        col_type = {}
-        # Get higher column count per var type
-        for key, value in result.items():
-            max_key = max(value, key=value.get)
-            col_type[key] = max_key
-
-        # Count columns per higher max type
-        result = {}
-        for key, value in col_type.items():
-            result[value] = result[value] + 1 if value in result else 1
-
-        return fill_missing_var_types(result)
-
-    def count_data_types(self, columns):
-        """
-        Count the number of int, float, strings and bool
+        Count the number of int, float, string and bool in a column
+        :param df:
         :param columns:
         :return:
         """
@@ -75,7 +53,7 @@ class Profiler:
             :param col_name:
             :return:
             """
-            types = self._df.cols().apply(col_name, check_data_type, "udf") \
+            types = df.cols().apply(col_name, check_data_type, "udf") \
                 .groupBy(col_name) \
                 .count()
 
@@ -90,7 +68,7 @@ class Profiler:
             results = fill_missing_var_types(results)
 
             # Subtract white spaces to the total string count
-            count_empty_strings = self._df.where(F.col(col_name) == '').count()
+            count_empty_strings = df.where(F.col(col_name) == '').count()
             results['string'] = results['string'] - count_empty_strings
 
             # List of returning values:
@@ -102,15 +80,62 @@ class Profiler:
                     "float": results['float']
                     }
 
-        columns = parse_columns(self._df, columns)
+        columns = parse_columns(df, columns)
 
         return {c: _count_data_types(c) for c in columns}
 
-    def columns(self, columns):
-        df = self._df
+    def sample_size(self, df):
+        count = df.count()
+        if count < 100:
+            fraction = 0.99
+        elif count < 1000:
+            fraction = 0.5
+        else:
+            fraction = 0.1
+        return fraction
 
+    def columns_by_data_types(self, columns):
+        """
+        Infer the column data type. Because the data in the column can be mixed(int ,float. string)
+        we just take a sample
+        :param columns:
+        :return:
+        """
+        columns = parse_columns(self._df, columns)
+
+        # Sample the df
+        # FIX: I am not sure which salpling method is better/faster in this case.
+        # We use sample(), other option could be sampleBy()
+        df = self._df
+        fraction = self.sample_size(df)
+
+        df_sampled = self._df.sample(False, fraction, seed=0).limit(1)
+
+        result = self.count_data_types(df_sampled, columns)
+
+        col_type = {}
+        # Get higher column count per var type
+        for key, value in result.items():
+            max_key = max(value, key=value.get)
+            col_type[key] = max_key
+
+        # Count columns per higher max type
+        result = {}
+        for key, value in col_type.items():
+            result[value] = result[value] + 1 if value in result else 1
+
+        return fill_missing_var_types(result)
+
+    def columns(self, columns):
+        """
+        Get statistical informarmation about a column
+        :param columns:
+        :return:
+        """
+        df = self._df
         columns = parse_columns(df, columns)
-        # Distinct, % , missing , %
+
+        # Initialize Objects
         column_info = {}
         column_info['columns'] = {}
 
@@ -122,9 +147,13 @@ class Profiler:
 
         na = df.cols().count_na(columns)
 
+        count_dtype = self.count_data_types(columns)
+
         for col_name in columns:
             col = {}
             # Check if column is numeric or categorical
+            cdt = count_dtype[col_name]
+            max(cdt, key=cdt.get)
 
             # Uniques
             col['uniques_count'] = uniques[col_name]
@@ -140,12 +169,8 @@ class Profiler:
             #                           .withColumn('%', F.col('count') / rows_count * 100).collect())
 
             col['frequency'] = collect_to_dict(df.select(F.col("num").alias("value")).groupBy("value").count()
-                                       .orderBy('count', ascending=False).limit(10)
-                                       .withColumn('percentage', F.col('count') / rows_count * 100).collect())
-
-            # print(col)
-            # print(column_info)
-            # print(col_name)
+                                               .orderBy('count', ascending=False).limit(10)
+                                               .withColumn('percentage', F.col('count') / rows_count * 100).collect())
 
             column_info['columns'][col_name] = col
 
