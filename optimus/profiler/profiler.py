7066cde9685d585ce62bdc6435356a8b0419ca5d
Argenis Leon <argenisleon@gmail.com>
2018-07-20 10:14:01 -0500

    Added qunatile and descriptive statistics

--- optimus/profiler/profiler.py
+++ optimus/profiler/profiler.py
@@ -58,33 +58,42 @@ class Profiler:
                 .count()
 
             # Convert the collect result to a list
-            # FIX: check if collect_to_dict function can be used here
+            # TODO: check if collect_to_dict function can be used here
 
-            results = {}
+            count_by_data_type = {}
             for row in types.collect():
-                results[row[0]] = row[1]
+                count_by_data_type[row[0]] = row[1]
 
             # Fill missing data types with 0
-            results = fill_missing_var_types(results)
+            count_by_data_type = fill_missing_var_types(count_by_data_type)
 
             # Subtract white spaces to the total string count
             count_empty_strings = df.where(F.col(col_name) == '').count()
-            results['string'] = results['string'] - count_empty_strings
+            count_by_data_type['string'] = count_by_data_type['string'] - count_empty_strings
 
-            # List of returning values:
-            return {"null": results['null'],
-                    "missing": count_empty_strings,
-                    "string": results['string'],
-                    "boolean": results['boolean'],
-                    "integer": results['integer'],
-                    "float": results['float']
-                    }
+            data_types_count = {"string": count_by_data_type['string'],
+                                "boolean": count_by_data_type['boolean'],
+                                "integer": count_by_data_type['integer'],
+                                "float": count_by_data_type['float']
+                                }
+            column_type = {"type": max(data_types_count, key=data_types_count.get)}
+
+            null_missed_count = {"null": count_by_data_type['null'],
+                                 "missing": count_empty_strings,
+                                 }
+
+            return {**data_types_count, **null_missed_count, **column_type}
 
         columns = parse_columns(df, columns)
 
         return {c: _count_data_types(c) for c in columns}
 
     def sample_size(self, df):
+        """
+        Get a size sample depending on the dataframe size
+        :param df:
+        :return:
+        """
         count = df.count()
         if count < 100:
             fraction = 0.99
@@ -104,9 +113,11 @@ class Profiler:
         columns = parse_columns(self._df, columns)
 
         # Sample the df
-        # FIX: I am not sure which salpling method is better/faster in this case.
+        # TODO: I am not sure which sampling method is better/faster in this case.
         # We use sample(), other option could be sampleBy()
         df = self._df
+
+        # Get a size sample depending on the dataframe size
         fraction = self.sample_size(df)
 
         df_sampled = self._df.sample(False, fraction, seed=0).limit(1)
@@ -128,7 +139,7 @@ class Profiler:
 
     def columns(self, columns):
         """
-        Get statistical informarmation about a column
+        Get statistical information about a column
         :param columns:
         :return:
         """
@@ -139,21 +150,21 @@ class Profiler:
         column_info = {}
         column_info['columns'] = {}
 
-        # Total
         rows_count = df.count()
         column_info['rows_count'] = rows_count
 
-        uniques = df.cols().count_uniques(columns)
-
-        na = df.cols().count_na(columns)
-
-        count_dtype = self.count_data_types(columns)
+        count_dtype = self.count_data_types(df, columns)
 
         for col_name in columns:
             col = {}
+
             # Check if column is numeric or categorical
-            cdt = count_dtype[col_name]
-            max(cdt, key=cdt.get)
+            column_type = count_dtype[col_name]['type']
+
+            # Get uniques
+            uniques = df.cols().count_uniques(col_name)
+
+            na = df.cols().count_na(col_name)
 
             # Uniques
             col['uniques_count'] = uniques[col_name]
@@ -163,14 +174,40 @@ class Profiler:
             col['missing_count'] = na[col_name]
             col['p_missing'] = na[col_name] / rows_count * 100
 
+            # Categorical column
+            if column_type == "string":
+                col['frequency'] = collect_to_dict(df.select(F.col("num").alias("value")).groupBy("value").count()
+                                                   .orderBy('count', ascending=False).limit(10)
+                                                   .withColumn('percentage',
+                                                               F.col('count') / rows_count * 100).collect())
+
+            # Numeric Column
+            elif column_type == "integer" or column_type == "float":
+                # Quantile statistics
+                min_value = df.cols().min(col_name)
+                max_value = df.cols().max(col_name)
+                col['min'] = min_value
+                col['max'] = max_value
+                col['quantile'] = df.cols().percentile(col_name, [0.05, 0.25, 0.5, 0.75, 0.95])
+                col['range'] = max_value - min_value
+                # q3 - q1
+
+                # Descriptive statistic
+                col['stdev'] = df.cols().std(col_name)
+                # Coef of variation
+                col['kurt'] = df.cols().kurt(col_name)
+                col['mean'] = df.cols().mean(col_name)
+                col['mad'] = df.cols().mad(col_name)
+                col['skewness'] = df.cols().skewness(col_name)
+                col['sum'] = df.cols().sum(col_name)
+                col['variance'] = df.cols().variance(col_name)
+
+                pass
+
+            elif column_type == "boolean":
+                print(column_type)
+
             # Buckets: values, count, %
-
-            # col['f'] = collect_to_dict(df.groupBy(col_name).count().orderBy('count', ascending=False).limit(10) \
-            #                           .withColumn('%', F.col('count') / rows_count * 100).collect())
-
-            col['frequency'] = collect_to_dict(df.select(F.col("num").alias("value")).groupBy("value").count()
-                                               .orderBy('count', ascending=False).limit(10)
-                                               .withColumn('percentage', F.col('count') / rows_count * 100).collect())
 
             column_info['columns'][col_name] = col
 
