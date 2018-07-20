a1a4eef3b0d4893022c7820459d8ae8d2a79d614
Argenis Leon <argenisleon@gmail.com>
2018-07-20 12:08:05 -0500

    print_json() added and sample_size() moved from profiler

--- optimus/profiler/functions.py
+++ optimus/profiler/functions.py
@@ -1,4 +1,5 @@
 from optimus.helpers.constants import *
+import json
 
 
 # Fill missing data types with 0
@@ -7,3 +8,27 @@ def fill_missing_var_types(var_types):
         if label not in var_types:
             var_types[label] = 0
     return var_types
+
+
+def sample_size(self, df):
+    """
+    Get a size sample depending on the dataframe size
+    :param df:
+    :return:
+    """
+    count = df.count()
+    if count < 100:
+        fraction = 0.99
+    elif count < 1000:
+        fraction = 0.5
+    else:
+        fraction = 0.1
+    return fraction
+
+
+def print_json(value):
+    """
+    Print beauty jsons
+    :return:
+    """
+    print(json.dumps(value, indent=2))
