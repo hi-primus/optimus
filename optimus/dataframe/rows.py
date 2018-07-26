4ce677f8a4cbd12106b721a11f6d36cc0c245347
Argenis Leon <argenisleon@gmail.com>
2018-07-26 00:33:56 -0500

    drop_na() and drop_duplicates() added

--- optimus/dataframe/rows.py
+++ optimus/dataframe/rows.py
@@ -1,7 +1,7 @@
 from pyspark.sql import DataFrame
 from pyspark.sql.dataframe import *
 
-from pyspark.sql import functions as F
+#from pyspark.sql import functions as F
 
 # Helpers
 import optimus.create as op
@@ -10,9 +10,6 @@ from optimus.helpers.constants import *
 from optimus.helpers.decorators import *
 
 from optimus.functions import filter_row_by_data_type as fbdt
-
-import builtins
-
 
 @add_method(DataFrame)
 def rows(self):
@@ -67,13 +64,19 @@ def rows(self):
 
     @add_attr(rows)
     def filter(*args, **kwargs):
+        """
+        Alias of Spark filter function. Return rows that match a expression
+        :param args:
+        :param kwargs:
+        :return:
+        """
         return self.filter(*args, **kwargs)
 
     @add_attr(rows)
     # https://chrisalbon.com/python/data_wrangling/pandas_dropping_column_and_rows/
     def drop(where=None):
         """
-        Drop a file depending
+        Drop a file depending dataframe expression
         :param where:
         :return:
         """
@@ -82,12 +85,44 @@ def rows(self):
     @add_attr(rows)
     def drop_by_dtypes(col_name, data_type=None):
         """
-
+        Drop rows by cell data type
         :param col_name:
         :param data_type:
         :return:
         """
         validate_columns_names(self, col_name)
-        return self.where(~fbdt(col_name, data_type))
+        return self.rows().drop(fbdt(col_name, data_type))
+
+    @add_attr(rows)
+    def drop_na(columns, how="all"):
+        """
+        Removes rows with null values. You can choose to drop the row if 'all' values are nulls or if
+        'any' of the values is null.
+
+        :param how: ‘any’ or ‘all’. If ‘any’, drop a row if it contains any nulls. If ‘all’, drop a row only if all its
+        values are null. The default is 'all'.
+        :return: Returns a new DataFrame omitting rows with null values.
+        """
+
+        assert isinstance(how, str), "Error, how argument provided must be a string."
+
+        assert how == 'all' or (
+                how == 'any'), "Error, how only can be 'all' or 'any'."
+
+        columns = parse_columns(self, columns)
+
+        return self.dropna(how, subset=columns)
+
+    @add_attr(rows)
+    def drop_duplicates(columns=None):
+        """
+        Drop duplicates values in a dataframe
+        :param columns: List of columns to make the comparison, this only  will consider this subset of columns,
+        for dropping duplicates. The default behavior will only drop the identical rows.
+        :return: Return a new DataFrame with duplicate rows removed
+        """
+
+        columns = parse_columns(self, columns)
+        return self.drop_duplicates(columns, subset=columns)
 
     return rows
