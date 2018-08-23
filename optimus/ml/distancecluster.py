78a6e6377c812ff360f6a3408ac3c5d41908e08d
Argenis Leon <argenisleon@gmail.com>
2018-08-23 12:24:10 -0500

    All functions moved outside the class. Classes removed

--- optimus/ml/distancecluster.py
+++ optimus/ml/distancecluster.py
@@ -4,103 +4,97 @@ from optimus.helpers.functions import collect_as_dict
 from optimus.ml.keycollision import KeyCollision
 
 
-class DistanceCluster:
+def levenshtein_matrix(df, col_name):
+    """
+    Create a couple of column with all the string combination
+    :param df:
+    :param col_name:
+    :return:
+    """
+    df = KeyCollision.fingerprint(df, col_name)
 
-    # def ppm Reference https://github.com/xiaowec/PPM-coding/blob/master/ppm.py
-    @staticmethod
-    def levenshtein_matrix(df, col_name):
-        """
-        Create a couple of column with all the string combination
-        :param df:
-        :param col_name:
-        :return:
-        """
-        df = KeyCollision.fingerprint(df, col_name)
+    col_fingerprint = col_name + "_FINGERPRINT"
+    col_distance = col_name + "_LEVENSHTEIN_DISTANCE"
 
-        col_fingerprint = col_name + "_FINGERPRINT"
-        col_distance = col_name + "_LEVENSHTEIN_DISTANCE"
+    temp_col_1 = col_name + "_LEVENSHTEIN_1"
+    temp_col_2 = col_name + "_LEVENSHTEIN_2"
 
-        temp_col_1 = col_name + "_LEVENSHTEIN_1"
-        temp_col_2 = col_name + "_LEVENSHTEIN_2"
+    # Prepare the columns to calculate the cross join
+    df = df.select(col_fingerprint).distinct().select(F.col(col_fingerprint).alias(temp_col_1),
+                                                      F.col(col_fingerprint).alias(temp_col_2))
 
-        # Prepare the columns to calculate the cross join
-        df = df.select(col_fingerprint).distinct().select(F.col(col_fingerprint).alias(temp_col_1),
-                                                          F.col(col_fingerprint).alias(temp_col_2))
+    #  Create all the combination between the string to calculate the levenshtein distance
+    df = df.select(temp_col_1).crossJoin(df.select(temp_col_2)) \
+        .withColumn(col_distance, F.levenshtein(F.col(temp_col_1), F.col(temp_col_2)))
 
-        #  Create all the combination between the string to calculate the levenshtein distance
-        df = df.select(temp_col_1).crossJoin(df.select(temp_col_2)) \
-            .withColumn(col_distance, F.levenshtein(F.col(temp_col_1), F.col(temp_col_2)))
-
-        return df
-
-    @staticmethod
-    def levenshtein_filter(df, col_name):
-        """
-        Get the nearest string
-        :param df:
-        :param col_name:
-        :return:
-        """
-        # TODO: must filter by and exprs
-        func = F.min
-
-        col_distance = col_name + "_LEVENSHTEIN_DISTANCE"
-        col_distance_r = col_name + "_LEVENSHTEIN_DISTANCE_R"
-
-        temp_col_1 = col_name + "_LEVENSHTEIN_1"
-        temp_col_2 = col_name + "_LEVENSHTEIN_2"
-        temp_r = "TEMP_R"
-
-        df = DistanceCluster.levenshtein_matrix(df, col_name)
-
-        # get the closest word
-        df_r = (df.rows.drop(F.col(col_distance) == 0)
-                .groupby(temp_col_1)
-                .agg(func(col_distance).alias(col_distance_r))
-                .cols.rename(temp_col_1, temp_r))
-
-        df = df.join(df_r, ((df_r[temp_r] == df[temp_col_1]) & (df_r[col_distance_r] == df[col_distance]))).select(
-            temp_col_1,
-            col_distance,
-            temp_col_2)
-
-        df = df \
-            .cols.rename([(temp_col_1, col_name + "_FROM"), (temp_col_2, col_name + "_TO")])
-
-        df.unpersist()
-        df_r.unpersist()
-
-        return df
-
-    @staticmethod
-    def levenshtein_cluster(df, col_name):
-        """
-        Return a dataframe with a cluster related to the string
-        :param df:
-        :param col_name:
-        :return:
-        """
-        # Prepare a group so we don need to apply the fingerprint to the whole data set
-        df = df.select(col_name).groupby(col_name).agg(F.count(col_name).alias("count"))
-
-        df = KeyCollision.fingerprint(df, col_name)
-
-        df_t = df.groupby(col_name + "_FINGERPRINT").agg(F.collect_list(col_name).alias("cluster"),
-                                                         F.size(F.collect_list(col_name)).alias("cluster_size"),
-                                                         F.first(col_name).alias("recommended"),
-                                                         F.sum("count").alias("count"))
-
-        # Filter min distance
-        df_l = DistanceCluster.levenshtein_filter(df, col_name)
-
-        # Cluster
-        df_l = df_l.join(df_t, (df_l[col_name + "_FROM"] == df_t[col_name + "_FINGERPRINT"]), how="left") \
-            .cols.drop(col_name + "_FINGERPRINT") \
-            .cols.drop([col_name + "_FROM", col_name + "_TO", col_name + "_LEVENSHTEIN_DISTANCE"])
-
-        return df_l
+    return df
 
 
-    def to_json(self, df, column):
-        return collect_as_dict(DistanceCluster.levenshtein_cluster(df, column).collect())
+def levenshtein_filter(df, col_name):
+    """
+    Get the nearest string
+    :param df:
+    :param col_name:
+    :return:
+    """
+    # TODO: must filter by and exprs
+    func = F.min
 
+    col_distance = col_name + "_LEVENSHTEIN_DISTANCE"
+    col_distance_r = col_name + "_LEVENSHTEIN_DISTANCE_R"
+
+    temp_col_1 = col_name + "_LEVENSHTEIN_1"
+    temp_col_2 = col_name + "_LEVENSHTEIN_2"
+    temp_r = "TEMP_R"
+
+    df = levenshtein_matrix(df, col_name)
+
+    # get the closest word
+    df_r = (df.rows.drop(F.col(col_distance) == 0)
+            .groupby(temp_col_1)
+            .agg(func(col_distance).alias(col_distance_r))
+            .cols.rename(temp_col_1, temp_r))
+
+    df = df.join(df_r, ((df_r[temp_r] == df[temp_col_1]) & (df_r[col_distance_r] == df[col_distance]))).select(
+        temp_col_1,
+        col_distance,
+        temp_col_2)
+
+    df = df \
+        .cols.rename([(temp_col_1, col_name + "_FROM"), (temp_col_2, col_name + "_TO")])
+
+    df.unpersist()
+    df_r.unpersist()
+
+    return df
+
+
+def levenshtein_cluster(df, col_name):
+    """
+    Return a dataframe with a string of cluster related to a string
+    :param df:
+    :param col_name:
+    :return:
+    """
+    # Prepare a group so we don need to apply the fingerprint to the whole data set
+    df = df.select(col_name).groupby(col_name).agg(F.count(col_name).alias("count"))
+    df = KeyCollision.fingerprint(df, col_name)
+
+    df_t = df.groupby(col_name + "_FINGERPRINT").agg(F.collect_list(col_name).alias("cluster"),
+                                                     F.size(F.collect_list(col_name)).alias("cluster_size"),
+                                                     F.first(col_name).alias("recommended"),
+                                                     F.sum("count").alias("count")).repartition(1)
+
+    # Filter nearest string
+    df_l = levenshtein_filter(df, col_name).repartition(1)
+
+    # Create Cluster
+    df_l = df_l.join(df_t, (df_l[col_name + "_FROM"] == df_t[col_name + "_FINGERPRINT"]), how="left") \
+        .cols.drop(col_name + "_FINGERPRINT") \
+        .cols.drop([col_name + "_FROM", col_name + "_TO", col_name + "_LEVENSHTEIN_DISTANCE"])
+
+    return df_l
+
+
+def to_json(df, column):
+    return collect_as_dict(levenshtein_cluster(df, column).collect())
--- optimus/ml/keycollision.py
+++ optimus/ml/keycollision.py
@@ -4,164 +4,156 @@ from pyspark.sql import functions as F
 from optimus.helpers.functions import parse_columns, collect_as_dict
 
 
-class KeyCollision:
+def fingerprint(df, columns):
     """
-    Taken for the amazing Open Refine post https://github.com/OpenRefine/OpenRefine/wiki/Clustering-In-Depth
+    Create the fingerprint for a
+    :param df:
+    :param columns:
+    :return:
     """
 
-    # def __init__(self, df):
-    #    self.df = df
-
-    @staticmethod
-    def fingerprint(df, columns):
+    def _split_sort_remove_join(value, args):
         """
-        Create the fingerprint for a
-        :param df:
-        :param columns:
+        Helper function to split, remove duplicates, sort and join back together
+        :param value:
+        :param args:
         :return:
         """
+        # Split into whitespace-separated token
+        split_key = value.split()
 
-        def _split_sort_remove_join(value, args):
-            """
-            Helper function to split, remove duplicates, sort and join back together
-            :param value:
-            :param args:
-            :return:
-            """
-            # Split into whitespace-separated token
-            split_key = value.split()
+        # Sort and remove duplicated items
+        sorted(set(split_key))
 
-            # Sort and remove duplicated items
-            sorted(set(split_key))
+        # join the tokens back together
+        return "".join(split_key)
 
-            # join the tokens back together
-            return "".join(split_key)
+    columns = parse_columns(df, columns)
+    for col_name in columns:
+        output_col = col_name + "_FINGERPRINT"
+        df = (df
+              .withColumn(output_col, F.col(col_name))
+              .cols.trim(output_col)
+              .cols.lower(output_col)
+              .cols.remove_special_chars(output_col)
+              .cols.remove_accents(output_col)
+              .cols.apply(output_col, _split_sort_remove_join, "string")
+              .repartition(1)
+              .cache()
+              )
+    return df
 
-        columns = parse_columns(df, columns)
-        for col_name in columns:
-            output_col = col_name + "_FINGERPRINT"
-            df = (df
-                  .withColumn(output_col, F.col(col_name))
-                  .cols.trim(output_col)
-                  .cols.lower(output_col)
-                  .cols.remove_special_chars(output_col)
-                  .cols.remove_accents(output_col)
-                  .cols.apply(output_col, _split_sort_remove_join, "string")
-                  .repartition(1)
-                  .cache()
-                  )
+
+def fingerprint_cluster(df, columns):
+    """
+    Cluster a dataframe column based on the Fingerprint algorithm
+    :param df:
+    :param columns: Columns to be processed
+    :return:
+    """
+    # df = self.df
+    columns = parse_columns(df, columns)
+
+    for col_name in columns:
+        output_col = col_name + "_FINGERPRINT"
+        # Instead of apply the fingerprint to the whole data set we group by names
+        df = (df
+              .groupBy(col_name)
+              .count()
+              .select('count', col_name)
+              .repartition(1)  # Needed for optimization in a single machine
+              .cache()
+              )
+        # Calculate the fingeprint
+        df = fingerprint(df, col_name)
+
+        # Create cluster
+        df = df.groupby(output_col).agg(
+            F.collect_set(col_name).alias("cluster"),
+            F.sum("count").alias("count"),
+            F.first(col_name).alias("recommended"),
+            F.size(F.collect_set(col_name)).alias("cluster_size")
+        ) \
+            .select("cluster_size", "cluster", "count", "recommended")
+    return df
+
+
+def n_gram_fingerprint(df, columns, n_size):
+    """
+    Calculate the ngram for a fingerprinted string
+    :param df:
+    :param columns:
+    :param n_size:
+    :return:
+    """
+
+    def remote_white_spaces_remove_sort_join(value, args):
+        # remove white spaces
+        value = [x.replace(" ", "") for x in value]
+
+        # sort and remove duplicated
+        value = sorted(set(value))
+
+        # join the tokens back together
+        value = "".join(value)
+
+        return value
+
+    columns = parse_columns(df, columns)
+    for col_name in columns:
+        output_col = col_name + "_NGRAM"
+        n_gram_col = col_name + "_NGRAM_FINGERPRINT"
+
+        df = (df
+              .withColumn(output_col, F.col(col_name))
+              .cols.lower(output_col)
+              .cols.remove_white_spaces(output_col)
+              .cols.remove_special_chars(output_col)
+              .cols.remove_accents(output_col)
+              # For create n-grams we need a Array type column
+              .cols.split(output_col, "")
+              .repartition(1)  # Needed for optimization in a single machine
+              .cache()
+              )
+
+        n_gram = NGram(n=n_size, inputCol=output_col, outputCol=n_gram_col)
+        df = n_gram.transform(df)
+        df = df.cols.apply(n_gram_col, remote_white_spaces_remove_sort_join, "string")
+
+    return df
+
+
+def n_gram_fingerprint_cluster(df, columns, n_size=2):
+    """
+    Cluster a DataFrame column based on the N-Gram Fingerprint algorithm
+    :param df:
+    :param columns:
+    :param n_size:
+    :return:
+    """
+    columns = parse_columns(df, columns)
+    for col_name in columns:
+        n_gram_col = col_name + "_ngram_fingerprint"
+
+        # Prepare a group so we don need to apply the fingerprint to the whole data set
+        df = (df.select(col_name)
+              .groupBy(col_name)
+              .count()
+              .select('count', col_name)
+              .repartition(1)  # Needed for optimization in a single machine
+              .cache())
+
+        df = n_gram_fingerprint(df, col_name, n_size)
+        # df.table()
+        df = df.groupby(n_gram_col).agg(
+            F.collect_set(col_name).alias("cluster"),
+            F.sum("count").alias("count"),
+            F.first(col_name).alias("recommended"),
+            F.size(F.collect_set(col_name)).alias("cluster_size")
+        ).select("cluster_size", "cluster", "count", "recommended")
+
         return df
 
-    @staticmethod
-    def fingerprint_cluster(df, columns):
-        """
-        Cluster a dataframe column based on the Fingerprint algorithm
-        :param df:
-        :param columns: Columns to be processed
-        :return:
-        """
-        # df = self.df
-        columns = parse_columns(df, columns)
 
-        for col_name in columns:
-            output_col = col_name + "_FINGERPRINT"
-            # Instead of apply the fingerprint to the whole data set we group by names
-            df = (df
-                  .groupBy(col_name)
-                  .count()
-                  .select('count', col_name)
-                  .repartition(1)  # Needed for optimization in a single machine
-                  .cache()
-                  )
-            # Calculate the fingeprint
-            df = KeyCollision.fingerprint(df, col_name)
-
-            # Create cluster
-            df = df.groupby(output_col).agg(
-                F.collect_set(col_name).alias("cluster"),
-                F.sum("count").alias("count"),
-                F.first(col_name).alias("recommended"),
-                F.size(F.collect_set(col_name)).alias("cluster_size")
-            ) \
-                .select("cluster_size", "cluster", "count", "recommended")
-        return df
-
-    @staticmethod
-    def n_gram_fingerprint(df, columns, n_size):
-        """
-        Calculate the ngram for a fingerprinted string
-        :param df:
-        :param columns:
-        :param n_size:
-        :return:
-        """
-
-        def remote_white_spaces_remove_sort_join(value, args):
-            # remove white spaces
-            value = [x.replace(" ", "") for x in value]
-
-            # sort and remove duplicated
-            value = sorted(set(value))
-
-            # join the tokens back together
-            value = "".join(value)
-
-            return value
-
-        columns = parse_columns(df, columns)
-        for col_name in columns:
-            output_col = col_name + "_NGRAM"
-            n_gram_col = col_name + "_NGRAM_FINGERPRINT"
-
-            df = (df
-                  .withColumn(output_col, F.col(col_name))
-                  .cols.lower(output_col)
-                  .cols.remove_white_spaces(output_col)
-                  .cols.remove_special_chars(output_col)
-                  .cols.remove_accents(output_col)
-                  # For create n-grams we need a Array type column
-                  .cols.split(output_col, "")
-                  .repartition(1)  # Needed for optimization in a single machine
-                  .cache()
-                  )
-
-            n_gram = NGram(n=n_size, inputCol=output_col, outputCol=n_gram_col)
-            df = n_gram.transform(df)
-            df = df.cols.apply(n_gram_col, remote_white_spaces_remove_sort_join, "string")
-
-        return df
-
-    @staticmethod
-    def n_gram_fingerprint_cluster(df, columns, n_size=2):
-        """
-        Cluster a DataFrame column based on the N-Gram Fingerprint algorithm
-        :param df:
-        :param columns:
-        :param n_size:
-        :return:
-        """
-        columns = parse_columns(df, columns)
-        for col_name in columns:
-            n_gram_col = col_name + "_ngram_fingerprint"
-
-            # Prepare a group so we don need to apply the fingerprint to the whole data set
-            df = (df.select(col_name)
-                  .groupBy(col_name)
-                  .count()
-                  .select('count', col_name)
-                  .repartition(1)  # Needed for optimization in a single machine
-                  .cache())
-
-            df = KeyCollision.n_gram_fingerprint(df, col_name, n_size)
-            # df.table()
-            df = df.groupby(n_gram_col).agg(
-                F.collect_set(col_name).alias("cluster"),
-                F.sum("count").alias("count"),
-                F.first(col_name).alias("recommended"),
-                F.size(F.collect_set(col_name)).alias("cluster_size")
-            ).select("cluster_size", "cluster", "count", "recommended")
-
-            return df
-
-    def to_json(self, df, column, n_size=2):
-        return collect_as_dict(self.n_gram_fingerprint_cluster(df, column, n_size).collect())
+def to_json(df, column, n_size=2):
+    return collect_as_dict(n_gram_fingerprint_cluster(df, column, n_size).collect())
