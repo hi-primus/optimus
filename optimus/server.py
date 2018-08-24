7baed040ef4aaaff0e1d2fac99c16f21a9a5c65b
Argenis Leon <argenisleon@gmail.com>
2018-08-23 21:32:46 -0500

    Update to handle unicode

--- optimus/server.py
+++ optimus/server.py
@@ -1,6 +1,5 @@
 import configparser
 import json
-from pathlib import Path
 
 from flask import Flask
 from flask import jsonify
@@ -8,6 +7,7 @@ from flask import jsonify
 app = Flask(__name__)
 
 config = configparser.ConfigParser()
+
 # try to load the config file
 try:
     config.read("config.ini")
@@ -16,34 +16,14 @@ except IOError:
     pass
 path = config["SERVER"]["Input"]
 
-print(Path.cwd())
-
 
 @app.route('/')
 def profiler_json():
     with app.app_context():
-        with open(path) as f:
-            data = json.load(f)
+        with open(path, encoding="utf8") as f:
+            data = json.loads(f.read())
             return jsonify(data)
 
 
 if __name__ == '__main__':
     app.run()
-
-
-class Server:
-    def __init__(self, output_path=None):
-        self.path = output_path
-        self.output_path = output_path
-
-        app = Flask("optimus")
-        app.run(debug=False)
-        app.route('/')(self.output_json)
-        self.app = app
-
-        # path = Path.cwd() / "data.json"
-
-    def output_json(self):
-        with open(self.path) as f:
-            data = json.load(f)
-        return jsonify(data)
