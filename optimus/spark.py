16bdd23e35d5e56b566f8812d93d9b438e02c286
Argenis Leon <argenisleon@gmail.com>
2018-08-26 01:11:17 -0500

    Fix. Call self.spark as property

--- optimus/spark.py
+++ optimus/spark.py
@@ -30,9 +30,11 @@ class Spark:
                 "Pyarrow not installed. Pandas UDF not available. Install using 'pip install pyarrow'")
         logging.info("-----")
         logging.info(STARTING_SPARK)
-        self.spark()
 
-    @lru_cache(maxsize=None)
+        # Build the spark session
+        self.spark
+
+
     @property
     def spark(self):
         """
@@ -53,4 +55,4 @@ class Spark:
         Return the Spark Context
         :return:
         """
-        return self.spark().sparkContext
+        return self.spark.sparkContext
