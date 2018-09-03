227f8c1fc1dffd1b23aeaa7f02be7f7ffebf2ad0
Argenis Leon <argenisleon@gmail.com>
2018-09-02 23:50:58 -0500

    Now optimus can handle jars, packages and repositories in the constructor. Examples added

--- optimus/optimus.py
+++ optimus/optimus.py
@@ -1,10 +1,11 @@
-import logging
 import os
+import sys
 from shutil import rmtree
 
 from optimus.create import Create
 from optimus.functions import concat
 from optimus.helpers.constants import *
+from optimus.helpers.functions import val_to_list
 from optimus.helpers.raiseit import RaiseIt
 from optimus.io.load import Load
 from optimus.ml.models import ML
@@ -17,7 +18,12 @@ Spark.instance = None
 class Optimus:
 
     def __init__(self, master="local[*]", app_name="optimus", checkpoint=False, path=None, file_system="local",
-                 verbose=False, dl=False):
+                 verbose=False, dl=False,
+                 repositories=None,
+                 packages=None,
+                 jars=None,
+                 options=None,
+                 additional_options=None):
 
         """
         Transform and roll out
@@ -26,24 +32,59 @@ class Optimus:
         :param path: path to the checkpoint folder
         :param checkpoint: If True create a checkpoint folder
         :param file_system: 'local' or 'hadoop'
-        """
+        :param additional_options:
 
-        if verbose is True:
-            logging.basicConfig(format="%(message)s", level=logging.INFO)
-        elif verbose is False:
-            logging.propagate = False
-            logging.disable(logging.NOTSET)
+        :param options: Configuration options that are passed to spark-submit.
+            See `the list of possible options
+            <https://spark.apache.org/docs/2.1.0/configuration.html#available-properties>`_.
+            Note that any options set already through PYSPARK_SUBMIT_ARGS will override
+            these.
+        :type options: (dict[str,str])
+        :param repositories: List of additional maven repositories for package lookup.
+        :type repositories: (list[str])
+
+        :param packages: Spark packages that should be installed.
+        :type packages: (list[str])
+
+        :param jars: Full paths to jar files that we want to include to the session.
+        :type jars: (list[str])
+
+        """
+        self.master = master
+        self.app_name = app_name
+
+        if options is None:
+            options = {}
+
+        self.options = options
+
+        if packages is None:
+            packages = []
+        else:
+            packages = val_to_list(packages)
+
+        self.packages = packages
+        self.repositories = repositories
+
+        if jars is None:
+            jars = {}
+
+        self.jars = jars
+        self.additional_options = additional_options
+
+        self.verbose(verbose)
 
         if dl is True:
-            Optimus.add_spark_packages(["databricks:spark-deep-learning:1.1.0-spark2.3-s_2.11 pyspark-shell"])
+            self._add_spark_packages(
+                ["databricks:spark-deep-learning:1.1.0-spark2.3-s_2.11", "com.databricks:spark-avro_2.11:4.0.0"])
 
-            Spark.instance = Spark(master, app_name)
+            self._start_session()
+
             from optimus.dl.models import DL
             self.dl = DL()
         else:
-
-            Spark.instance = Spark(master, app_name)
-            pass
+            self._add_spark_packages(["com.databricks:spark-avro_2.11:4.0.0"])
+            self._start_session()
 
         if path is None:
             path = os.getcwd()
@@ -60,7 +101,7 @@ class Optimus:
 
         logging.info(STARTING_OPTIMUS)
         if checkpoint is True:
-            self.set_check_point_folder(path, file_system)
+            self._set_check_point_folder(path, file_system)
 
         logging.info(SUCCESS)
 
@@ -86,7 +127,8 @@ class Optimus:
         """
         return Spark.instance.sc
 
-    def stop(self):
+    @staticmethod
+    def stop():
         """
         Stop Spark Session
         :return:
@@ -94,16 +136,7 @@ class Optimus:
         Spark.instance.spark.stop()
 
     @staticmethod
-    def add_spark_packages(packages):
-        """
-        Define the Spark packages that must be loaded at start time
-        :param packages:
-        :return:
-        """
-        os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages " + " ".join(packages)
-
-    @staticmethod
-    def set_check_point_folder(path, file_system):
+    def _set_check_point_folder(path, file_system):
         """
         Function that receives a workspace path where a folder is created.
         This folder will store temporal dataframes when user writes the .checkPoint().
@@ -181,4 +214,91 @@ class Optimus:
 
     @staticmethod
     def concat(dfs, like):
+        """
+        Concat multiple dataframes
+        :param dfs: List of Dataframes
+        :param like: concat as columns or rows
+        :return:
+        """
         return concat(dfs, like)
+
+    def _add_spark_packages(self, packages):
+        """
+        Define the Spark packages that must be loaded at start time
+        :param packages:
+        :return:
+        """
+        for p in packages:
+            self.packages.append(p)
+
+    def _setup_repositories(self):
+        if self.repositories:
+            return '--repositories {}'.format(','.join(self.repositories))
+        else:
+            return ''
+
+    def _setup_packages(self):
+        if self.packages:
+            return '--packages {}'.format(','.join(self.packages))
+        else:
+            return ''
+
+    def _setup_jars(self):
+        if self.jars:
+            return '--jars {}'.format(','.join(self.jars))
+        else:
+            return ''
+
+    def _setup_options(self, additional_options):
+        options = {}
+
+        options.update(self.options)
+
+        if additional_options:
+            options.update(additional_options)
+
+        if 'spark.sql.catalogImplementation' not in options:
+            options['spark.sql.catalogImplementation'] = 'hive'
+
+        # Here we massage conf properties with the intent to pass them to
+        # spark-submit; this is convenient as it is unified with the approach
+        # we take for repos, packages and jars, and it also handles precedence
+        # of conf properties already defined by the user in a very
+        # straightforward way (since we always append to PYSPARK_SUBMIT_ARGS)
+        return ' '.join('--conf "{}={}"'.format(*o) for o in sorted(options.items()))
+
+    def _start_session(self):
+        """
+
+        :return:
+        """
+        ## Get python.exe fullpath
+        os.environ['PYSPARK_PYTHON'] = sys.executable
+
+        submit_args = [
+            # options that were already defined through PYSPARK_SUBMIT_ARGS
+            # take precedence over SparklySession's
+            os.environ.get('PYSPARK_SUBMIT_ARGS', '').replace('pyspark-shell', ''),
+            self._setup_repositories(),
+            self._setup_packages(),
+            self._setup_jars(),
+            self._setup_options(self.additional_options),
+            'pyspark-shell',
+        ]
+
+        env = ' '.join(filter(None, submit_args))
+        os.environ['PYSPARK_SUBMIT_ARGS'] = env
+        Spark.instance = Spark(self.master, self.app_name)
+
+    @staticmethod
+    def verbose(verbose):
+        """
+        Enable verbose mode
+        :param verbose:
+        :return:
+        """
+        if verbose is True:
+            logging.basicConfig(format="%(message)s", level=logging.INFO)
+        elif verbose is False:
+            logging.propagate = False
+            logging.disable(logging.NOTSET)
