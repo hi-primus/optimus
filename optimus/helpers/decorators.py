71043a9931b0fd401f82e59356993497357cafee
Argenis Leon <argenisleon@gmail.com>
2018-09-04 03:20:30 -0500

    Add decorator to track a function execution time

--- optimus/helpers/decorators.py
+++ optimus/helpers/decorators.py
@@ -1,9 +1,12 @@
+import timeit
+import time
+import logging
 from functools import wraps
 
 
 def add_method(cls):
     """
-    Use it as a decorator to add a function to specific class
+    Attach a function to a class as an attribute
     :param cls: Class in which the function will be attached
     :return:
     """
@@ -19,11 +22,35 @@ def add_method(cls):
     return decorator
 
 
-def add_attr(cls):
+def time_it(method):
+    def timed(*args, **kw):
+        start_time = timeit.default_timer()
+        f = method(*args, **kw)
+        _time = round(timeit.default_timer() - start_time, 2)
+        logging.info("{name}() executed in {time} sec".format(name=method.__name__, time=_time))
+        return f
+
+    return timed
+
+
+def add_attr(cls, log_time=False):
+    """
+    Attach a function to another functions as an attribute
+    :param cls: class where the function will be attached
+    :param log_time: Print the execution time. Verbose must be true on the Optimus constructor
+    :type log_time: bool
+    :return:
+    """
+
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
+            start_time = timeit.default_timer()
+            f = func(*args, **kwargs)
+            _time = round(timeit.default_timer() - start_time, 2)
+            if log_time:
+                logging.info("{name}() executed in {time} sec".format(name=func.__name__, time=_time))
+            return f
 
         setattr(cls, func.__name__, wrapper)
         return func
