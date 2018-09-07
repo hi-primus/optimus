4b945dcf00322eb3c2e6fdfc4ef76213754e7eb8
Argenis Leon <argenisleon@gmail.com>
2018-09-07 15:02:52 -0500

    Move all code inside try catch block

--- optimus/server/run.py
+++ optimus/server/run.py
@@ -12,6 +12,34 @@ try:
     config.read("config.ini")
     path = config["SERVER"]["Input"]
     app = Flask(__name__)
+
+
+    @app.route('/')
+    def index():
+        """
+        Return a message indicating if the server is running.
+        :return:
+        """
+        return jsonify("Optimus Server is running... Go to /profiler to get the Optimus profiler data.")
+
+
+    @app.route('/profiler')
+    def profiler():
+        """
+        Return the data profiler in json format.
+        :return:
+        """
+        try:
+            with app.app_context():
+                with open(path, encoding="utf8") as f:
+                    data = json.loads(f.read())
+                    return jsonify(data)
+        except IOError:
+            return jsonify("Not data profiling available")
+
+
+    app.run()
+
 except IOError:
     print("config.ini not found")
 
@@ -19,30 +47,3 @@ except KeyError:
     print("Input info not found in config.ini. Be sure you have...")
     print("[SERVER]")
     print("Input = config.ini")
-
-
-@app.route('/')
-def index():
-    """
-    Return a message indicating if the server is running.
-    :return:
-    """
-    return jsonify("Optimus Server is running... Go to /profiler to get the Optimus profiler data.")
-
-
-@app.route('/profiler')
-def profiler():
-    """
-    Return the data profiler in json format.
-    :return:
-    """
-    try:
-        with app.app_context():
-            with open(path, encoding="utf8") as f:
-                data = json.loads(f.read())
-                return jsonify(data)
-    except IOError:
-        return jsonify("Not data profiling available")
-
-
-app.run()
