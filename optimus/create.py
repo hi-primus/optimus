6688bdf08497008e6340ac1f1b1d2ad1d39f388d
Argenis Leon <argenisleon@gmail.com>
2018-09-12 18:48:25 -0500

    Additional fixes

--- optimus/create.py
+++ optimus/create.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pyspark.sql.types import StructField, StructType, StringType
 
 # Helpers
-from optimus.helpers.checkit import is_tuple, is_list_of_tuples, is_
+from optimus.helpers.checkit import is_tuple, is_, is_list, is_one_element, is_list_of_tuples
 from optimus.helpers.functions import get_spark_dtypes_object
 from optimus.spark import Spark
 
@@ -15,37 +15,38 @@ class Create:
         Helper to create a Spark dataframe:
         :param cols: List of Tuple with name, data type and a flag to accept null
         :param rows: List of Tuples if vals with the same number and types that cols
-        :param pdf:
+        :param pdf: a pandas dataframe
         :return: Dataframe
         """
         if is_(pdf, pd.DataFrame):
             result = Spark.instance.spark.createDataFrame(pdf)
         else:
+
+            specs = []
+            # Process the rows
             if not is_list_of_tuples(rows):
                 rows = [(i,) for i in rows]
 
-            specs = []
-
+            # Process the columns
             for c in cols:
-
                 # Get columns name
-                if not is_tuple(c):
+
+                if is_one_element(c):
                     col_name = c
-                else:
-                    col_name = c[0]
-
-                # Get columns data type
-                if len(c) >= 2:
-                    var_type = get_spark_dtypes_object(c[1])
-                else:
                     var_type = StringType()
-
-                # Get column nullable flag. It's just to tell if a column accept nulls as values
-                if len(c) == 3:
-                    nullable = c[2]
-                    var_type = get_spark_dtypes_object(c[1])
-                else:
                     nullable = True
+
+                elif is_tuple(c):
+
+                    # Get columns data type
+                    col_name = c[0]
+                    var_type = get_spark_dtypes_object(c[1])
+
+                    count = len(c)
+                    if count == 2:
+                        nullable = True
+                    elif count == 3:
+                        nullable = c[2]
 
                 # If tuple has not the third param with put it to true to accepts Null in columns
                 specs.append([col_name, var_type, nullable])
