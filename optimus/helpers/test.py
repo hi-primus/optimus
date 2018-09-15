44fe503bb273be4008d0fed76158d0bf534bda4a
Argenis Leon <argenisleon@gmail.com>
2018-09-14 23:24:06 -0500

    Now a global dataframe and imports can be defined in the constructor

--- optimus/helpers/test.py
+++ optimus/helpers/test.py
@@ -2,25 +2,56 @@ from optimus.helpers.checkit import is_str, is_list_empty
 
 
 class Test:
-    def __init__(self):
-        self.buffer = []
+    def __init__(self, df=None, name=None, imports=None):
+        """
+        Create python code with unit test for Optimus.
+        :param df: Spark Dataframe
+        :param name: Name of the Test Class
+        :param imports: Libraries to be added
 
-        pass
+        """
 
-    def add(self, value):
-        self.buffer.append(value)
+        self.df = df
+        self.name = name
+        self.imports = imports
 
     def run(self, *args):
+
         """
-        Print all the results
-        :param args:
+        Return the tests in text format
+        :param args: list of create functions
         :return:
         """
-        self.add("from pyspark.sql.types import *\n")
-        self.add("from optimus import Optimus\n")
+        filename = "test_" + self.name + ".py"
+        test_file = open(filename, 'w')
 
-        for a in args:
-            print(a)
+        _imports = [
+            "from pyspark.sql.types import *",
+            "from optimus import Optimus"
+        ]
+        if self.imports is not None:
+            for i in self.imports:
+                _imports.append(i)
+
+        # Imports
+        for i in _imports:
+            test_file.write(i + "\n")
+
+        test_file.write("op = Optimus()\n")
+
+        # Global Dataframe
+        if self.df is not None:
+            source_df = "source_df=op.create.df(" + self.df.export() + ")\n"
+            test_file.write(source_df)
+
+        # Class name
+        cls = "class Test" + self.name + "(object):\n"
+
+        test_file.write(cls)
+        for t in args:
+            test_file.write(t)
+
+        test_file.close()
 
     def create(self, df, func, output, *args, **kwargs):
         """
@@ -33,14 +64,22 @@ class Test:
         :return:
         """
 
+        buffer = []
+
+        def add_buffer(value):
+            buffer.append("\t" + value)
+
         func_test_name = "test_" + func.replace(".", "_") + "()"
 
-        self.add("@staticmethod")
-        self.add("def " + func_test_name + ":\n")
+        add_buffer("@staticmethod\n")
+        add_buffer("def " + func_test_name + ":\n")
 
-        source_df = "\tsource_df=op.create.df(" + df.export() + ")\n"
-
-        self.add(source_df)
+        if df is not None:
+            source_df = "\tsource_df=op.create.df(" + df.export() + ")\n"
+            method_to_call = df
+            add_buffer(source_df)
+        else:
+            method_to_call = self.df
 
         # Process simple arguments
         _args = []
@@ -60,10 +99,10 @@ class Test:
         if (not is_list_empty(args)) & (not is_list_empty(kwargs)):
             separator = ","
 
-        self.add("\tactual_df = source_df." + func + "(" + _args + separator + ','.join(_kwargs) + ")\n")
+        add_buffer("\tactual_df = source_df." + func + "(" + _args + separator + ','.join(_kwargs) + ")\n")
 
         # Process functions
-        method_to_call = df
+
         for f in func.split("."):
             method_to_call = getattr(method_to_call, f)
 
@@ -76,16 +115,16 @@ class Test:
                 df_result = "'" + df_result + "'"
             else:
                 df_result = str(df_result)
-            expected = "\texpected_json =" + df_result + "\n"
+            expected = "\texpected_value =" + df_result + "\n"
 
-        self.add(expected)
+        add_buffer(expected)
 
         if output == "df":
-            self.add("\tassert (expected_df.collect() == actual_df.collect())\n")
+            add_buffer("\tassert (expected_df.collect() == actual_df.collect())\n")
         elif output == "json":
-            self.add("\tassert (expected_json == actual_df)\n")
+            add_buffer("\tassert (expected_value == actual_df)\n")
 
-        self.add(func_test_name + "\n")
+        # add_buffer(func_test_name + "\n")
 
         if False:
             print("-----------")
@@ -99,3 +138,4 @@ class Test:
                 df_result.table()
             elif output == "json":
                 print(df_result)
+        return "".join(buffer)
