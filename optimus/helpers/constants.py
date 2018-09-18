c54479cf0bd1103662f6205ba67feef5228119c3
Argenis Leon <argenisleon@gmail.com>
2018-09-17 20:26:33 -0500

    Added bigint, date, bytem short

--- optimus/helpers/constants.py
+++ optimus/helpers/constants.py
@@ -1,7 +1,23 @@
 import logging
 
 from pyspark.sql.types import StringType, BooleanType, IntegerType, ArrayType, FloatType, DoubleType, StructType, \
-    DateType, LongType
+    DateType, LongType, ByteType, ShortType
+
+"""
+Python to PySpark reference
+
+type(None): NullType,
+bool: BooleanType,
+int: LongType,
+float: DoubleType,
+str: StringType,
+bytearray: BinaryType,
+decimal.Decimal: DecimalType,
+datetime.date: DateType,
+datetime.datetime: TimestampType,
+datetime.time: TimestampType,
+
+"""
 
 PYTHON_SHORT_TYPES = {"string": "string",
                       "str": "string",
@@ -14,12 +30,10 @@ PYTHON_SHORT_TYPES = {"string": "string",
                       "array": "array",
                       "null": "null"
                       }
-PYTHON_TYPES_ = {"string": str, "int": int, "float": float, "boolean": bool}
+PYTHON_TYPES = {"string": str, "int": int, "float": float, "boolean": bool}
 
-PROFILER_TYPES = {"int", "float", "string", "bool", "date", "null", "array"}
-PROFILER_LEGEND_TYPES = {"string": "ABC", "int": "#", "integer": "#", "float": "##.#", "double": "##.#", "bigint": "#"}
-
-PROFILER_COLUMN_TYPES = {"categorical", "numeric", "date", "bool", "null", "array"}
+PYSPARK_NUMERIC_TYPES = ["byte", "short", "big", "int", "double", "float"]
+PYSPARK_NOT_ARRAY_TYPE = ["byte", "short", "big", "int", "double", "float", "double", "string", "date", "bool"]
 
 SPARK_SHORT_DTYPES = {"string": "string",
                       "str": "string",
@@ -35,6 +49,8 @@ SPARK_SHORT_DTYPES = {"string": "string",
                       "struct": "struct",
                       "array": "array",
                       "date": "date",
+                      "byte": "byte",
+                      "short": "short"
                       # "vector": "vector"
 
                       }
@@ -42,16 +58,19 @@ SPARK_SHORT_DTYPES = {"string": "string",
 SPARK_DTYPES_DICT_OBJECTS = {"string": StringType(), "int": IntegerType(), "float": FloatType(),
                              "double": DoubleType(), "boolean": BooleanType(), "struct": StructType(),
                              "array": ArrayType, "bigint": LongType(),
-                             "date": DateType()
+                             "date": DateType(), "byte": ByteType(), "short": ShortType()
                              }
 
 SPARK_DTYPES_DICT = {"string": StringType, "int": IntegerType, "float": FloatType,
                      "double": DoubleType, "boolean": BooleanType, "struct": StructType, "array": ArrayType,
-                     "date": DateType, "bigint": LongType
+                     "bigint": LongType,
+                     "date": DateType, "byte": ByteType, "short": ShortType
                      }
 
-# TODO: We should get all the data types from pyspark.sql.types something like tuple(t.__all__)
-SPARK_DTYPES = (StringType, IntegerType, FloatType, DoubleType, ArrayType)
+# Profiler
+PROFILER_TYPES = {"int", "float", "string", "bool", "date", "null", "array"}
+PROFILER_LEGEND_TYPES = {"string": "ABC", "int": "#", "integer": "#", "float": "##.#", "double": "##.#", "bigint": "#"}
+PROFILER_COLUMN_TYPES = {"categorical", "numeric", "date", "bool", "null", "array"}
 
 # Strings and Function Messages
 JUST_CHECKING = "Just check that Spark and all necessary environments vars are present..."
