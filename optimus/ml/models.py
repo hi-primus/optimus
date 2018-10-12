3a681ce29f9fe862ec7911c8be3ea00f7aee0d8b
Favio André Vázquez <favio.vazquezp@gmail.com>
2018-10-12 18:23:57 -0500

    Fix intentation and import for deep learning estimator in h2o

--- optimus/ml/models.py
+++ optimus/ml/models.py
@@ -6,7 +6,7 @@ from optimus.helpers.functions import parse_columns
 from optimus.ml.feature import string_to_index, vector_assembler
 
 from pysparkling import *
-from pysparkling.ml import H2OAutoML
+from pysparkling.ml import H2OAutoML, H2ODeepLearning
 from pyspark.sql.functions import *
 from pyspark.sql.session import SparkSession
 
@@ -152,21 +152,21 @@ class ML:
     @staticmethod
     def h2o_deeplearning(df, label, columns, **kargs):
         
-    H2OContext.getOrCreate(spark)
-    
-    df_sti = fe.string_to_index(df, input_cols=label)
-    df_va  =  fe.vector_assembler(df_sti, input_cols=columns)
-    h2o_deeplearning = H2ODeepLearning(epochs=10,
-                         seed=1,
-                         l1=0.001,
-                         l2=0.0,
-                         hidden=[200, 200],
-                         featuresCols=columns,
-                         predictionCol=label,
-                         **kargs)
-    model = h2o_deeplearning.fit(df_va)
-    df_raw = model.transform(df_va)
-    
-    df_pred = df_raw.withColumn("prediction", when(df_raw.prediction_output["p1"] > 0.5, 1.0).otherwise(0.0))
-    
-    return df_pred, model
+        H2OContext.getOrCreate(spark)
+
+        df_sti = fe.string_to_index(df, input_cols=label)
+        df_va  =  fe.vector_assembler(df_sti, input_cols=columns)
+        h2o_deeplearning = H2ODeepLearning(epochs=10,
+                             seed=1,
+                             l1=0.001,
+                             l2=0.0,
+                             hidden=[200, 200],
+                             featuresCols=columns,
+                             predictionCol=label,
+                             **kargs)
+        model = h2o_deeplearning.fit(df_va)
+        df_raw = model.transform(df_va)
+
+        df_pred = df_raw.withColumn("prediction", when(df_raw.prediction_output["p1"] > 0.5, 1.0).otherwise(0.0))
+
+        return df_pred, model
