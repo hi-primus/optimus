45c655d56c0d8fce9806dfcd29754910ba11a6b4
Favio André Vázquez <favio.vazquezp@gmail.com>
2018-10-13 02:18:06 -0500

    Fix test for h2o gbm

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -117,8 +117,8 @@ def test_h2o_xgboost():
 
     assert isinstance(xgboost_model, py_sparkling.ml.models.H2OXGBoostModel), "Not a H2OXGBoostModel"
 
-def test_h2o_gbmn():
-    df_model, xgboost_model = op.ml.h2o_gbm(df_cancer, "diagnosis", columns_h2o)
+def test_h2o_gbm():
+    df_model, gbm_model = op.ml.h2o_gbm(df_cancer, "diagnosis", columns_h2o)
 
     assert_spark_df(df_model)
 
