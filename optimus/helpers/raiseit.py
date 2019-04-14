339458108b0a899688dcc679f4d113ccbee523a4
Argenis Leon <argenisleon@gmail.com>
2019-04-13 21:46:01 -0500

    Added length error exeption

--- optimus/helpers/raiseit.py
+++ optimus/helpers/raiseit.py
@@ -2,14 +2,13 @@ class RaiseIt:
 
     @staticmethod
     def type_error(var, data_types):
-        from optimus.helpers.functions import get_var_name
         """
         Raise a TypeError exception
         :param var:
         :param types:data types as strings
         :return:
         """
-
+        from optimus.helpers.functions import get_var_name
         if len(data_types) == 1:
             divisor = ""
         elif len(data_types) == 2:
@@ -25,17 +24,17 @@ class RaiseIt:
 
     @staticmethod
     def value_error(var, data_values):
-        from optimus.helpers.functions import get_var_name
         """
         Raise a ValueError exception
         :param var:
         :param _list: list of values accepted
         :return:
         """
+        from optimus.helpers.functions import get_var_name
 
         if len(data_values) == 1:
             divisor = ""
-        if len(data_values) == 2:
+        elif len(data_values) == 2:
             divisor = " or "
         elif len(data_values) > 2:
             divisor = ", "
@@ -49,12 +48,24 @@ class RaiseIt:
                                      data_values)), var_type=var))
 
     @staticmethod
-    def type(cls, var, message):
+    def length_error(var, length):
+        """
+        Raise a ValueError exception when the var length is nor correct
+        :param var:
+        :param length: Expected var length
+        :return:
+        """
         from optimus.helpers.functions import get_var_name
+        raise ValueError("'{var_name}' must be {length}, received '{var_length}'"
+                         .format(var_name=get_var_name(var),
+                                 length=length, var_length=len(var)))
 
+    @staticmethod
+    def type(cls, var, message):
         """
         Raise and exception ot type specified
         :param var:
         :return:
         """
+        from optimus.helpers.functions import get_var_name
         raise cls("'{var_name}' error".format(var_name=get_var_name(var), var_type=var))
