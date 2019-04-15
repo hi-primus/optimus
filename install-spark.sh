352c71046caf671f688435944314677b23e31373
Argenis Leon <argenisleon@gmail.com>
2019-04-14 22:10:32 -0500

    Changed install spark path to the apache archive

--- install-spark.sh
+++ install-spark.sh
@@ -4,5 +4,5 @@ set -e
 SPARK_VERSION='2.4.1'
 HADOOP_VERSION='2.7'
 
-curl http://apache.mirror.gtcomm.net/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz --output /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
+curl https://archive.apache.org/dist/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz --output /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
 cd /tmp && tar -xvzf /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
