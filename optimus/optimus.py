582d50f4fa652dee83ae6a1743b3ab70eab1a148
Argenis Leon <argenisleon@gmail.com>
2019-04-29 13:55:36 -0500

    Fix on css path

--- optimus/optimus.py
+++ optimus/optimus.py
@@ -162,8 +162,8 @@ class Optimus:
             if __IPYTHON__:
                 # # Load the Jinja template
 
-                path = os.path.dirname(os.path.abspath(__file__))                
-                url = path + "//style//css.css"
+                path = os.path.dirname(os.path.abspath(__file__))
+                url = path + "//css//styles.css"
                 styles = open(url, "r").read()
                 s = '<style>%s</style>' % styles
                 print_html(s)
