e76109ab4422aaa9977b66f4429c3aee5e8a26ed
Favio Vazquez <favio.vazquezp@gmail.com>
2019-05-20 17:53:23 -0500

    Test finding sparkdl 3

--- tests/test_dl.py
+++ tests/test_dl.py
@@ -1,6 +1,6 @@
 from optimus import Optimus
+from optimus.dl.models import DL
 import pyspark
-from pyspark.ml.image import ImageSchema
 from pyspark.sql.functions import lit
 
 import os
@@ -8,8 +8,7 @@ SUBMIT_ARGS = "--packages databricks:spark-deep-learning:1.5.0-spark2.4-s_2.11 p
 os.environ["PYSPARK_SUBMIT_ARGS"] = SUBMIT_ARGS
 
 op = Optimus()
-from sparkdl.image import imageIO
-
+dl = DL()
 
 # rdd = op.sc.parallelize([Row(predicted_labels=['daisy', '0.8918145298957825']),
 #                         Row(predicted_labels=['picket_fence', '0.14247830212116241']),
@@ -26,26 +25,26 @@ def assert_spark_model(model):
     assert isinstance(model, pyspark.ml.PipelineModel), "Not a model"
 
 
-tulips_df = ImageSchema.readImages("tests/testtulips/").withColumn("label", lit(1))
-daisy_df = imageIO.readImagesWithCustomFn("tests/testdaisy/", decode_f=imageIO.PIL_decode).withColumn("label", lit(0))
+tulips_df = op.spark.read.format("image").load("optimus/tests/testtulips/").withColumn("label", lit(1))
+daisy_df = op.spark.read.format("image").load("optimus/tests/testdaisy/").withColumn("label", lit(0))
 
 train_df = tulips_df.unionAll(daisy_df)
 
 
 def test_image_classifier_lr():
-    model, df_preds = op.dl.image_classifier_lr(train_df)
+    model, df_preds = dl.image_classifier_lr(train_df)
 
     assert_spark_model(model)
     assert_spark_df(df_preds)
 
 
 def test_evaluate_img_lr():
-    model, df_preds = op.dl.image_classifier_lr(train_df)
-    result = op.dl.evaluate_image_classifier(train_df, model)
+    model, df_preds = dl.image_classifier_lr(train_df)
+    result = dl.evaluate_image_classifier(train_df, model)
 
     assert isinstance(result, float), "Not a float"
 
 
 def test_image_predictor():
-    preds = op.dl.image_predictor("tests/sampleimg/")
+    preds = dl.image_predictor("tests/sampleimg/")
     assert_spark_df(preds)
