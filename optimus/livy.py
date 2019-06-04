b931586f9d7fe2ca057f1cfa097c83cbb0dc8e81
Argenis Leon <argenisleon@gmail.com>
2019-06-04 16:27:46 -0400

    Added result() and change to execute() to submit()

--- optimus/livy.py
+++ optimus/livy.py
@@ -1,16 +1,21 @@
 import json
+import textwrap
 
 import requests
 
 HEADERS = {'Content-Type': 'application/json'}
 START = {'kind': 'pyspark'}
+TIMEOUT = 10
 
+
+# Reference https://github.com/apache/incubator-livy/tree/master/python-api/src/main/python/livy
 
 class Livy:
     def __init__(self, host):
         self.host = host
         self.session_id = None
         self.session_url = None
+        self.statement_id = None
 
     def start(self):
         """
@@ -30,18 +35,40 @@ class Livy:
         url = "{HOST}/sessions/{SESSION_ID}".format(HOST=self.host, SESSION_ID=self.session_id)
         return self.send(url, type="GET")
 
-    def execute(self, data):
+    def submit(self, code):
         """
-        Sens a statemrn to livy
-        :param data:
+        Send code to livy
+        :param code:
         :return:
         """
 
-        url = "{HOST}/sessions/{SESSION_ID}/statements".format(HOST=self.host, SESSION_ID=self.session_id, timeout=10)
+        data = {'code': textwrap.dedent(code)}
 
-        return self.send(url, data=data, type="POST")
+        url = "{HOST}/sessions/{SESSION_ID}/statements".format(HOST=self.host, SESSION_ID=self.session_id)
 
+        r = self.send(url, data=data, type="POST")
+
+        self.statement_id = r["id"]
+
+        filter_string = ["id", "output", "progress", "state"]
+        return {k: v for (k, v) in r.items() if k in filter_string}
         # r.headers['location']
+
+    def result(self, id=None):
+        """
+
+        :param id:
+        :return:
+        """
+        if id is None:
+            id = self.statement_id
+        url = "{HOST}/sessions/{SESSION_ID}/statements/{STATEMENT_ID}".format(HOST=self.host,
+                                                                              SESSION_ID=self.session_id,
+                                                                              STATEMENT_ID=id)
+
+        r = self.send(url, type="GET")
+        if r["output"]["status"] == "ok":
+            return r["output"]["data"]["text/plain"]
 
     @staticmethod
     def send(url, data=None, type="GET"):
