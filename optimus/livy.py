71af4a11636bb80e3325d548c6c8d11947d1f1f5
Argenis Leon <argenisleon@gmail.com>
2019-06-08 19:44:04 -0400

    livy improvements

--- optimus/livy.py
+++ optimus/livy.py
@@ -3,6 +3,8 @@ import textwrap
 
 import requests
 
+from optimus import RaiseIt
+
 HEADERS = {'Content-Type': 'application/json'}
 START = {'kind': 'pyspark'}
 TIMEOUT = 10
@@ -41,19 +43,22 @@ class Livy:
         :return:
         """
 
+        self.session()["state"] ==""
         data = {'code': textwrap.dedent(code)}
         url = "{HOST}/sessions/{SESSION_ID}/statements".format(HOST=self.host, SESSION_ID=self.session_id)
 
         r = self.send(url, data=data, type="POST")
+        if "id" in r:
+            self.statement_id = r["id"]
 
-        self.statement_id = r["id"]
-
-        filter_string = ["id", "output", "progress", "state"]
-        return {k: v for (k, v) in r.items() if k in filter_string}
+            filter_string = ["id", "output", "progress", "state"]
+            return {k: v for (k, v) in r.items() if k in filter_string}
+        else:
+            print("Can not submit the job. Verify the Session")
 
     def result(self, id=None):
         """
-
+        Return the result from a statement
         :param id:
         :return:
         """
@@ -62,21 +67,26 @@ class Livy:
         url = "{HOST}/sessions/{SESSION_ID}/statements/{STATEMENT_ID}".format(HOST=self.host,
                                                                               SESSION_ID=self.session_id,
                                                                               STATEMENT_ID=id)
-
         r = self.send(url, type="GET")
-        if r["output"]["status"] == "ok":
-            return r["output"]["data"]["text/plain"]
+        print(r)
+        if "output" in r:
+            if r["output"] is not None:
+                if r["output"]["status"] == "ok":
+                    return r["output"]["data"]["text/plain"]
+        else:
+            print(r)
+
+
 
     @staticmethod
     def send(url, data=None, type="GET"):
         """
         Helper to manage http requests
-        :param url:
-        :param data:
-        :param type:
+        :param url: Url
+        :param data: Data to be send
+        :param type: GET, POST or DELETE
         :return:
         """
-        # url = self.host + '/sessions'
 
         try:
             if type == "GET":
@@ -86,6 +96,8 @@ class Livy:
             elif type == "DELETE":
                 return requests.delete(url, data=json.dumps(data), headers=HEADERS, verify=False,
                                        timeout=TIMEOUT).json()
+            else:
+                RaiseIt.value_error(type, ["GET", "POST", "DELETE"])
 
         except requests.exceptions.HTTPError as e:
             print("Error: " + str(e))
