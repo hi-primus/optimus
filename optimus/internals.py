8c8c842ce1c7bad8bc6c070ae2e5037918fee4c3
Argenis Leon <argenisleon@gmail.com>
2019-07-08 23:01:41 -0500

    Standardize column naming

--- optimus/internals.py
+++ optimus/internals.py
@@ -1,9 +1,9 @@
 def _z_score_col_name(col_name):
-    return "z_col_" + col_name
+    return col_name + "z_col_"
 
 
 def _m_z_score_col_name(col_name):
-    return "m_z_col_" + col_name
+    return col_name + "m_z_col_"
 
 
 def _bucket_col_name(col_name):
