98e5d882588d82a4e62a7e3546c91f6564fac1e4
Argenis Leon <argenisleon@gmail.com>
2019-07-25 12:27:06 -0500

    Fix #544

--- optimus/helpers/check.py
+++ optimus/helpers/check.py
@@ -340,7 +340,7 @@ def is_int(value):
 
 def is_url(value):
     regex = re.compile(
-        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'^(?:http|ftp|hdfs)s?://'  # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
         r'localhost|'  # localhost...
         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
