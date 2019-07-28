93e21cd248ba7a26f36be78d58f494bfe1d82fdb
Argenis Leon <argenisleon@gmail.com>
2019-07-28 15:05:16 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     author='Favio Vazquez and Argenis Leon',
     author_email='favio.vazquez@ironmussa.com',
     url='https://github.com/ironmussa/Optimus/',
-    download_url='https://github.com/ironmussa/Optimus/archive/2.2.7.tar.gz',
+    download_url='https://github.com/ironmussa/Optimus/archive/2.2.9.tar.gz',
     description=('Optimus is the missing framework for cleaning and pre-processing data in a distributed fashion with '
                  'pyspark.'),
     long_description=readme(),
