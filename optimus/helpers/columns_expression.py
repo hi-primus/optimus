a733b8fa3883fcadf686666e632de2fac2da5987
Argenis Leon <argenisleon@gmail.com>
2019-08-11 12:00:23 -0500

    Revert "Quality improvements"
    This reverts commit 1dcde021be5cb251797cce2dc8e80e08cc8500fd.

--- optimus/helpers/columns_expression.py
+++ optimus/helpers/columns_expression.py
@@ -8,8 +8,10 @@ from optimus.helpers.constants import PYSPARK_NUMERIC_TYPES, PYSPARK_STRING_TYPE
 from optimus.helpers.converter import val_to_list
 from optimus.helpers.functions import create_buckets
 
+"""
+These function can return and Column Expression or a list of columns expression
 
-# These function can return and Column Expression or a list of columns expression
+"""
 
 
 def match_nulls_strings(col_name):
@@ -41,6 +43,7 @@ def zeros_agg(col_name):
 
 
 def hist_agg(col_name, df, buckets):
+
     """
     Create a columns expression to calculate a column histogram
     :param col_name:
