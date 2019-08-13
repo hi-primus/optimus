0a1d535570a1ba96669bd7cc22040a87d5711e65
Argenis Leon <argenisleon@gmail.com>
2019-08-13 00:14:36 -0500

    Minimal chages

--- optimus/__init__.py
+++ optimus/__init__.py
@@ -1,4 +1,5 @@
 import os
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
 
 # pyspark_pipes: build Spark ML pipelines easily
 from .ml.pipelines import patch
@@ -14,7 +15,6 @@ from optimus.io import save
 from optimus.profiler.profiler import Profiler
 from optimus.bumblebee import Comm
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
 
 # Handle encoding problem
 # https://stackoverflow.com/questions/39662384/pyspark-unicodeencodeerror-ascii-codec-cant-encode-character
