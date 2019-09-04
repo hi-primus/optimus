df5dd6f343d68bcb6e2b0205d90f5ebe6a52a200
Mariano Gonzalez <eschizoid@hotmail.com>
2019-09-03 20:23:50 -0500

    Added Cassandra support

---
+++ optimus/io/driver.py
@@ -0,0 +1,32 @@
+from enum import Enum, unique
+
+
+@unique
+class DriverResolver(Enum):
+    """Enum holding all driver properties."""
+
+    CASSANDRA = {"name": "cassandra", "port": 9042}
+    MY_SQL = {"name": "mysql", "port": 3306}
+    ORACLE = {"name": "oracle", "port": 1521, "class": "oracle.jdbc.OracleDriver"}
+    POSTGRES = {"name": "postgres", "port": 5432, "class": "org.postgresql.Driver"}
+    POSTGRES_SQL = {"name": "postgresql", "port": 5432, "class": "org.postgresql.Driver"}
+    SQL_SERVER = {"name": "sqlserver", "port": 1433}
+    SQL_LITE = {"name": "sqlite", "port": 0}
+    PRESTO = {"name": "presto", "port": 8080, "class": "com.facebook.presto.jdbc.PrestoDriver"}
+    REDSHIFT = {"name": "redshift", "port": 5439}
+
+    def __str__(self):
+        return self.value["name"]
+
+    def port(self):
+        return self.value["port"]
+
+    def java_class(self):
+        return self.value["class"]
+
+    def describe(self):
+        return self.value["name"], self.value["port"], self.value["class"]
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
