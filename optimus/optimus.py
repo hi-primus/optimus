25ad5b6dd3e22d71ff752ae85cc4dc6f55dd53c1
Argenis Leon <argenisleon@gmail.com>
2019-09-11 13:57:22 -0500

    Change class definition by function

--- optimus/optimus.py
+++ optimus/optimus.py
@@ -1,12 +1,10 @@
 from optimus.engines.spark import SparkEngine
 
 
-class Optimus:
-    @staticmethod
-    def start(engine, *args, **kwargs):
-        if engine == "spark":
-            return SparkEngine(*args, **kwargs)
-        elif engine == "pandas":
-            print("not implemented yet")
-        elif engine == "rapids":
-            print("not implemented yet")
+def optimus(engine, *args, **kwargs):
+    if engine == "spark":
+        return SparkEngine(*args, **kwargs)
+    elif engine == "pandas":
+        print("not implemented yet")
+    elif engine == "rapids":
+        print("not implemented yet")
