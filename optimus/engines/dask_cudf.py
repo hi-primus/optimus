124218b53156a7679c7dafa679762199c3be9f2a
Argenis Leon <argenisleon@gmail.com>
2019-11-18 19:32:35 -0600

    WIP added support for dask cudf

---
+++ optimus/engines/dask_cudf.py
@@ -0,0 +1,27 @@
+from dask.distributed import Client
+from dask_cuda import LocalCUDACluster
+
+from optimus.bumblebee import Comm
+from optimus.dask.dask import Dask
+from optimus.dask.io.load import Load
+from optimus.profiler.profiler import Profiler
+
+Dask.instance = None
+Profiler.instance = None
+Comm.instance = None
+
+
+class DaskCUDFEngine:
+    def __init__(self, *args, **kwargs):
+        self.engine = 'dask_cudf'
+        # self.create = Create()
+        self.load = Load()
+        # self.read = self.spark.read
+
+        cluster = LocalCUDACluster()
+        client = Client(cluster)
+
+        Dask.instance = client
+
+        Profiler.instance = Profiler()
+        self.profiler = Profiler.instance
--- optimus/optimus.py
+++ optimus/optimus.py
@@ -5,9 +5,9 @@ def optimus(engine="spark", *args, **kwargs):
     elif engine == "dask":
         from optimus.engines.dask import DaskEngine
         return DaskEngine(*args, **kwargs)
-    # elif engine == "pandas":
-    #     from optimus.engines.pandas import PandasEngine
-    #     return PandasEngine(*args, **kwargs)
-
     elif engine == "dask_cudf":
-        raise NotImplementedError('dask_cudf')
+        from optimus.engines.dask_cudf import DaskCUDFEngine
+        return DaskCUDFEngine(*args, **kwargs)
+    elif engine == "pandas":
+        from optimus.engines.pandas import PandasEngine
+        return PandasEngine(*args, **kwargs)
