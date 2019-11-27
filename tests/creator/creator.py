f6c3903915fe162830bd99d27baa17ade821ecef
Argenis Leon <argenisleon@gmail.com>
2019-11-27 13:23:00 -0600

    Fix test

--- tests/creator/creator.py
+++ tests/creator/creator.py
@@ -803,6 +803,56 @@ source_df.table()
 
 # # Ouliers
 
+# +
+import pandas as pd
+from pyspark.sql.types import *
+from datetime import date, datetime
+
+
+cols = [
+        ("names", "str"),
+        ("height(ft)", ShortType()),
+        ("function", "str"),
+        ("rank", ByteType()),
+        ("age", "int"),
+        ("weight(t)", "float"),
+        "japanese name",
+        "last position seen",
+        "date arrival",
+        "last date seen",
+        ("attributes", ArrayType(FloatType())),
+        ("Date Type", DateType()),
+        ("timestamp", TimestampType()),
+        ("Cybertronian", BooleanType()),
+        ("function(binary)", BinaryType()),
+        ("NullType", NullType())
+
+    ]
+
+rows = [
+        ("Optim'us", -28, "Leader", 10, 5000000, 4.30, ["Inochi", "Convoy"], "19.442735,-99.201111", "1980/04/10",
+         "2016/09/10", [8.5344, 4300.0], date(2016, 9, 10), datetime(2014, 6, 24), True, bytearray("Leader", "utf-8"),
+         None),
+        ("bumbl#ebéé  ", 17, "Espionage", 7, 5000000, 2.0, ["Bumble", "Goldback"], "10.642707,-71.612534", "1980/04/10",
+         "2015/08/10", [5.334, 2000.0], date(2015, 8, 10), datetime(2014, 6, 24), True, bytearray("Espionage", "utf-8"),
+         None),
+        ("ironhide&", 26, "Security", 7, 5000000, 4.0, ["Roadbuster"], "37.789563,-122.400356", "1980/04/10",
+         "2014/07/10", [7.9248, 4000.0], date(2014, 6, 24), datetime(2014, 6, 24), True, bytearray("Security", "utf-8"),
+         None),
+        ("Jazz", 13, "First Lieutenant", 8, 5000000, 1.80, ["Meister"], "33.670666,-117.841553", "1980/04/10",
+         "2013/06/10", [3.9624, 1800.0], date(2013, 6, 24), datetime(2014, 6, 24), True,
+         bytearray("First Lieutenant", "utf-8"), None),
+        ("Megatron", None, "None", 10, 5000000, 5.70, ["Megatron"], None, "1980/04/10", "2012/05/10", [None, 5700.0],
+         date(2012, 5, 10), datetime(2014, 6, 24), True, bytearray("None", "utf-8"), None),
+        ("Metroplex_)^$", 300, "Battle Station", 8, 5000000, None, ["Metroflex"], None, "1980/04/10", "2011/04/10",
+         [91.44, None], date(2011, 4, 10), datetime(2014, 6, 24), True, bytearray("Battle Station", "utf-8"), None),
+        (None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None),
+
+    ]
+source_df = op.create.df(cols ,rows)
+source_df.table()
+# -
+
 t = Test(op, source_df, "df_outliers", imports=["from pyspark.ml.linalg import Vectors, VectorUDT, DenseVector",
                                         "import numpy as np",
                                         "nan = np.nan",
--- tests/test_df_outliers.py
+++ tests/test_df_outliers.py
@@ -1,6 +1,8 @@
 from pyspark.sql.types import *
 from optimus import Optimus
-from optimus.helpers.json import json_enconding 
+from optimus.helpers.json import json_enconding
+from optimus.helpers.functions import deep_sort
+import unittest
 from pyspark.ml.linalg import Vectors, VectorUDT, DenseVector
 import numpy as np
 nan = np.nan
@@ -8,7 +10,8 @@ import datetime
 from pyspark.sql import functions as F
 op = Optimus(master='local')
 source_df=op.create.df([('names', StringType(), True),('height(ft)', ShortType(), True),('function', StringType(), True),('rank', ByteType(), True),('age', IntegerType(), True),('weight(t)', FloatType(), True),('japanese name', ArrayType(StringType(),True), True),('last position seen', StringType(), True),('date arrival', StringType(), True),('last date seen', StringType(), True),('attributes', ArrayType(FloatType(),True), True),('Date Type', DateType(), True),('timestamp', TimestampType(), True),('Cybertronian', BooleanType(), True),('function(binary)', BinaryType(), True),('NullType', NullType(), True)], [("Optim'us", -28, 'Leader', 10, 5000000, 4.300000190734863, ['Inochi', 'Convoy'], '19.442735,-99.201111', '1980/04/10', '2016/09/10', [8.53439998626709, 4300.0], datetime.date(2016, 9, 10), datetime.datetime(2014, 6, 24, 0, 0), True, bytearray(b'Leader'), None), ('bumbl#ebéé  ', 17, 'Espionage', 7, 5000000, 2.0, ['Bumble', 'Goldback'], '10.642707,-71.612534', '1980/04/10', '2015/08/10', [5.334000110626221, 2000.0], datetime.date(2015, 8, 10), datetime.datetime(2014, 6, 24, 0, 0), True, bytearray(b'Espionage'), None), ('ironhide&', 26, 'Security', 7, 5000000, 4.0, ['Roadbuster'], '37.789563,-122.400356', '1980/04/10', '2014/07/10', [7.924799919128418, 4000.0], datetime.date(2014, 6, 24), datetime.datetime(2014, 6, 24, 0, 0), True, bytearray(b'Security'), None), ('Jazz', 13, 'First Lieutenant', 8, 5000000, 1.7999999523162842, ['Meister'], '33.670666,-117.841553', '1980/04/10', '2013/06/10', [3.962399959564209, 1800.0], datetime.date(2013, 6, 24), datetime.datetime(2014, 6, 24, 0, 0), True, bytearray(b'First Lieutenant'), None), ('Megatron', None, 'None', 10, 5000000, 5.699999809265137, ['Megatron'], None, '1980/04/10', '2012/05/10', [None, 5700.0], datetime.date(2012, 5, 10), datetime.datetime(2014, 6, 24, 0, 0), True, bytearray(b'None'), None), ('Metroplex_)^$', 300, 'Battle Station', 8, 5000000, None, ['Metroflex'], None, '1980/04/10', '2011/04/10', [91.44000244140625, None], datetime.date(2011, 4, 10), datetime.datetime(2014, 6, 24, 0, 0), True, bytearray(b'Battle Station'), None), (None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None)])
-class Testdf_outliers(object):
+class Test_df_outliers(unittest.TestCase):
+	maxDiff = None
 	@staticmethod
 	def test_outliers_mad_count():
 		actual_df =source_df.outliers.mad('height(ft)',0.5,10000).count()
@@ -69,25 +72,25 @@ class Testdf_outliers(object):
 	def test_outliers_tukey_count():
 		actual_df =source_df.outliers.tukey('height(ft)').count()
 		actual_df =json_enconding(actual_df)
-		expected_value =json_enconding(7)
-		assert (expected_value == actual_df)
+		expected_value =json_enconding(5)
+		assert(expected_value == actual_df)
 	@staticmethod
 	def test_outliers_tukey_drop():
 		actual_df =source_df.outliers.tukey('height(ft)').drop()
-		expected_df = op.create.df([('names', StringType(), True),('height(ft)', ShortType(), True),('function', StringType(), True),('rank', ByteType(), True),('age', IntegerType(), True),('weight(t)', FloatType(), True),('japanese name', ArrayType(StringType(),True), True),('last position seen', StringType(), True),('date arrival', StringType(), True),('last date seen', StringType(), True),('attributes', ArrayType(FloatType(),True), True),('Date Type', DateType(), True),('timestamp', TimestampType(), True),('Cybertronian', BooleanType(), True),('function(binary)', BinaryType(), True),('NullType', NullType(), True)], [('bumbl#ebéé  ', 17, 'Espionage', 7, 5000000, 2.0, ['Bumble', 'Goldback'], '10.642707,-71.612534', '1980/04/10', '2015/08/10', [5.334000110626221, 2000.0], datetime.date(2015, 8, 10), datetime.datetime(2014, 6, 24, 0, 0), True, bytearray(b'Espionage'), None), ('ironhide&', 26, 'Security', 7, 5000000, 4.0, ['Roadbuster'], '37.789563,-122.400356', '1980/04/10', '2014/07/10', [7.924799919128418, 4000.0], datetime.date(2014, 6, 24), datetime.datetime(2014, 6, 24, 0, 0), True, bytearray(b'Security'), None), ('Jazz', 13, 'First Lieutenant', 8, 5000000, 1.7999999523162842, ['Meister'], '33.670666,-117.841553', '1980/04/10', '2013/06/10', [3.962399959564209, 1800.0], datetime.date(2013, 6, 24), datetime.datetime(2014, 6, 24, 0, 0), True, bytearray(b'First Lieutenant'), None)])
+		expected_df = op.create.df([('names', StringType(), True),('height(ft)', ShortType(), True),('function', StringType(), True),('rank', ByteType(), True),('age', IntegerType(), True),('weight(t)', FloatType(), True),('japanese name', ArrayType(StringType(),True), True),('last position seen', StringType(), True),('date arrival', StringType(), True),('last date seen', StringType(), True),('attributes', ArrayType(FloatType(),True), True),('Date Type', DateType(), True),('timestamp', TimestampType(), True),('Cybertronian', BooleanType(), True),('function(binary)', BinaryType(), True),('NullType', NullType(), True)], [])
 		assert (expected_df.collect() == actual_df.collect())
 	@staticmethod
 	def test_outliers_tukey_info():
 		actual_df =source_df.outliers.tukey('height(ft)').info()
 		actual_df =json_enconding(actual_df)
-		expected_value =json_enconding({'count_outliers': 7, 'count_non_outliers': 3, 'lower_bound': -6.5, 'upper_bound': 45.5, 'iqr1': 13, 'iqr3': 26})
-		assert (expected_value == actual_df)
+		expected_value =json_enconding({'count_outliers': 5, 'count_non_outliers': 2, 'lower_bound': 45.5, 'lower_bound_count': 4, 'upper_bound': -6.5, 'upper_bound_count': 4, 'q1': 13, 'median': 17, 'q3': 26, 'iqr': 13})
+		assert(expected_value == actual_df)
 	@staticmethod
 	def test_outliers_tukey_non_outliers_count():
 		actual_df =source_df.outliers.tukey('height(ft)').non_outliers_count()
 		actual_df =json_enconding(actual_df)
-		expected_value =json_enconding(3)
-		assert (expected_value == actual_df)
+		expected_value =json_enconding(2)
+		assert(expected_value == actual_df)
 	@staticmethod
 	def test_outliers_tukey_select():
 		actual_df =source_df.outliers.tukey('height(ft)').select()
@@ -97,14 +100,14 @@ class Testdf_outliers(object):
 	def test_outliers_tukey_whiskers():
 		actual_df =source_df.outliers.tukey('height(ft)').whiskers()
 		actual_df =json_enconding(actual_df)
-		expected_value =json_enconding({'lower_bound': -6.5, 'upper_bound': 45.5, 'iqr1': 13, 'iqr3': 26})
-		assert (expected_value == actual_df)
+		expected_value =json_enconding({'lower_bound': -6.5, 'upper_bound': 45.5, 'q1': 13, 'median': 17, 'q3': 26, 'iqr': 13})
+		assert(expected_value == actual_df)
 	@staticmethod
 	def test_outliers_z_score_count():
 		actual_df =source_df.outliers.z_score('height(ft)',0.5).count()
 		actual_df =json_enconding(actual_df)
 		expected_value =json_enconding(2)
-		assert (expected_value == actual_df)
+		assert(expected_value == actual_df)
 	@staticmethod
 	def test_outliers_z_score_drop():
 		actual_df =source_df.outliers.z_score('height(ft)',0.5).drop()
@@ -115,13 +118,13 @@ class Testdf_outliers(object):
 		actual_df =source_df.outliers.z_score('height(ft)',0.5).info()
 		actual_df =json_enconding(actual_df)
 		expected_value =json_enconding({'count_outliers': 2, 'count_non_outliers': 3, 'max_z_score': 1.76684})
-		assert (expected_value == actual_df)
+		assert(expected_value == actual_df)
 	@staticmethod
 	def test_outliers_z_score_non_outliers_count():
 		actual_df =source_df.outliers.z_score('height(ft)',0.5).non_outliers_count()
 		actual_df =json_enconding(actual_df)
 		expected_value =json_enconding(3)
-		assert (expected_value == actual_df)
+		assert(expected_value == actual_df)
 	@staticmethod
 	def test_outliers_z_score_select():
 		actual_df =source_df.outliers.z_score('height(ft)',0.5).select()
