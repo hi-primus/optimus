397a1c4612a3e72815e10a2308a000297f62897d
Argenis Leon <argenisleon@gmail.com>
2020-01-28 01:09:51 -0600

    WIP dask cudf

--- optimus/engines/dask_cudf/engine.py
+++ optimus/engines/dask_cudf/engine.py
@@ -4,6 +4,7 @@ from dask_cuda import LocalCUDACluster
 from optimus.bumblebee import Comm
 from optimus.engines.dask.dask import Dask
 from optimus.engines.dask.io.load import Load
+from optimus.helpers.logger import logger
 from optimus.profiler.profiler import Profiler
 
 Dask.instance = None
@@ -12,7 +13,7 @@ Comm.instance = None
 
 
 class DaskCUDFEngine:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, verbose=False, comm=None, *args, **kwargs):
         self.engine = 'dask_cudf'
         # self.create = Create()
         self.load = Load()
@@ -21,9 +22,25 @@ class DaskCUDFEngine:
         cluster = LocalCUDACluster()
         client = Client(cluster)
 
+        self.engine = 'dask_cudf'
+        # self.create = Create()
+        self.load = Load()
+        # self.read = self.spark.read
+        self.verbose(verbose)
+
         Dask.instance = client
 
         self.client = Dask.instance
 
         Profiler.instance = Profiler()
         self.profiler = Profiler.instance
+
+    @staticmethod
+    def verbose(verbose):
+        """
+        Enable verbose mode
+        :param verbose:
+        :return:
+        """
+
+        logger.active(verbose)
