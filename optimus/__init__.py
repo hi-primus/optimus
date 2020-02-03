f98a4cc03d4beecdb2ec5acf33251e3050c92385
Argenis Leon <argenisleon@gmail.com>
2020-02-02 21:22:45 -0600

    Import depends on engine selected

--- optimus/__init__.py
+++ optimus/__init__.py
@@ -1,29 +1,14 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
-# from .optimus import Optimus
 
-# SPARK MONKEY PATCH
-# pyspark_pipes: build Spark ML pipelines easily
-from optimus.engines.spark.ml.pipelines import patch
-# from .optimus import *
 from .optimus import optimus as Optimus
-
-patch()
-
-from .engines.spark import rows, columns, extension, constants, functions
-
-from . import meta
-from .plots import plots
-from .outliers import outliers
-from .engines.spark.io import save
 
 # Handle encoding problem
 # https://stackoverflow.com/questions/39662384/pyspark-unicodeencodeerror-ascii-codec-cant-encode-character
 
+# Preserve compatibility with <3.x brach
 os.environ["PYTHONIOENCODING"] = "utf8"
 
-# DASK MONKEY PATCH
-from .engines.dask import columns, rows, constants, extension, functions
-from .engines.dask.io import save
-# Preserve compatibility with <3.x brach
+
+
