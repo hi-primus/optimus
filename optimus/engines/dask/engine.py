00b8672c9e4eec923916c003af3d4476eee21020
Argenis Leon <argenisleon@gmail.com>
2020-04-21 12:15:10 -0500

    Update to 4GB by default

--- optimus/engines/dask/engine.py
+++ optimus/engines/dask/engine.py
@@ -16,7 +16,8 @@ Comm.instance = None
 class DaskEngine:
     __version__ = __version__
 
-    def __init__(self, session=None, n_workers=1, threads_per_worker=None, processes=False, memory_limit='2GB',
+    # Using procces or threads https://stackoverflow.com/questions/51099685/best-practices-in-setting-number-of-dask-workers
+    def __init__(self, session=None, n_workers=1, threads_per_worker=None, processes=False, memory_limit='4GB',
                  verbose=False, comm=None, *args, **kwargs):
         if comm is True:
             Comm.instance = Comm()
@@ -27,13 +28,13 @@ class DaskEngine:
 
         if n_workers is None:
             import psutil
-            threads_per_worker = psutil.cpu_count()*4
+            threads_per_worker = psutil.cpu_count() * 4
         # self.create = Create()
         self.load = Load()
         self.verbose(verbose)
 
         if session is None:
-            print("PROCESS", processes)
+            # print("PROCESS", processes)
             Dask.instance = Client(n_workers=n_workers, threads_per_worker=threads_per_worker, processes=processes,
                                    memory_limit=memory_limit, *args,
                                    **kwargs)
