a3cfe7ceff7b2932228965f822742a2f79f96b63
Argenis Leon <argenisleon@gmail.com>
2020-04-28 16:41:37 -0500

    Move engines handling to a Enum

--- optimus/optimus.py
+++ optimus/optimus.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from optimus.helpers.logger import logger
 from optimus.helpers.raiseit import RaiseIt
 from optimus.meta import meta
@@ -19,7 +21,20 @@ class MetadataDask:
         self._metadata = value
 
 
-def optimus(engine="dask", *args, **kwargs):
+class Engine(Enum):
+    PANDAS = "pandas"
+    CUDF = "cudf"
+    DASK = "dask"
+    DASK_CUDF = "dask_cudf"
+    SPARK = "spark"
+    VAEX = "vaex"
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+
+
+def optimus(engine=Engine.DASK.value, *args, **kwargs):
     logger.print("ENGINE", engine)
     # Monkey Patching
     import pandas as pd
@@ -33,7 +48,7 @@ def optimus(engine="dask", *args, **kwargs):
     PandasDataFrame.meta = property(meta)
     PandasDataFrame.schema = [MetadataDask()]
 
-    if engine == "spark":
+    if engine == Engine.SPARK.value:
         from pyspark.sql import DataFrame as SparkDataFrame
 
         # pyspark_pipes: build Spark ML pipelines easily
@@ -46,7 +61,7 @@ def optimus(engine="dask", *args, **kwargs):
         SparkDataFrame.outliers = property(outliers)
         SparkDataFrame.meta = property(meta)
 
-    if engine == "dask" or engine == "dask-cudf":
+    if engine == Engine.DASK.value or engine == Engine.DASK_CUDF.value:
         from dask.dataframe.core import DataFrame as DaskDataFrame
 
         from optimus.engines.dask import columns, rows, extension, functions
@@ -57,7 +72,7 @@ def optimus(engine="dask", *args, **kwargs):
         DaskDataFrame.meta = property(meta)
         DaskDataFrame.schema = [MetadataDask()]
 
-    if engine == "cudf":
+    if engine == Engine.CUDF.value:
         from cudf.core import DataFrame as CUDFDataFrame
 
         from optimus.engines.cudf import columns, rows, extension, functions, constants
@@ -68,9 +83,9 @@ def optimus(engine="dask", *args, **kwargs):
         CUDFDataFrame.meta = property(meta)
         CUDFDataFrame.schema = [MetadataDask()]
 
-    if engine == "dask-cudf":
+    if engine == Engine.DASK_CUDF.value:
         from dask_cudf.core import DataFrame as DaskCUDFDataFrame
-
+        print("ASDFADSF")
         from optimus.engines.dask_cudf import columns, rows, extension, functions
         from optimus.engines.base.dask import constants
         from optimus.engines.dask_cudf.io import save
@@ -79,7 +94,7 @@ def optimus(engine="dask", *args, **kwargs):
         DaskCUDFDataFrame.meta = property(meta)
         DaskCUDFDataFrame.schema = [MetadataDask()]
 
-    if engine == "vaex":
+    if engine == Engine.VAEX.value:
         from vaex import DataFrame as VaexDataFrame
         # import pandas as pd
         from optimus.engines.vaex import rows, columns, extension, constants, functions
@@ -90,29 +105,31 @@ def optimus(engine="dask", *args, **kwargs):
         VaexDataFrame.schema = [MetadataDask()]
 
     # Dummy so pycharm not complain about not used imports
-    columns, rows, constants, extension, functions, save, plots
+
     # else:
     #     RaiseIt.value_error(engine, ["spark", "cudf", "dask-cudf"])
+    columns, rows, constants, extension, functions, save, plots
 
     # Init engine
-    if engine == "pandas":
+    if engine == Engine.PANDAS.value:
         from optimus.engines.pandas.engine import PandasEngine
         return PandasEngine(*args, **kwargs)
 
-    elif engine == "spark":
+    elif engine == Engine.SPARK.value:
         from optimus.engines.spark.engine import SparkEngine
         return SparkEngine(*args, **kwargs)
 
-    elif engine == "dask":
+    elif engine == Engine.DASK.value:
         from optimus.engines.dask.engine import DaskEngine
         return DaskEngine(*args, **kwargs)
 
-    elif engine == "cudf":
+    elif engine == Engine.CUDF.value:
         from optimus.engines.cudf.engine import CUDFEngine
         return CUDFEngine(*args, **kwargs)
 
-    elif engine == "dask_cudf":
+    elif engine == Engine.DASK_CUDF.value:
         from optimus.engines.dask_cudf.engine import DaskCUDFEngine
         return DaskCUDFEngine(*args, **kwargs)
     else:
-        RaiseIt.value_error(engine, ["pandas","spark", "dask", "cudf", "dask_cudf"])
+        print("ASDAf", Engine.list())
+        RaiseIt.value_error(engine, Engine.list())
