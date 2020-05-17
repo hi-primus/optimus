c53cbaca85e17cadb7643385a0c0cccdc92b7b33
Argenis Leon <argenisleon@gmail.com>
2020-05-17 17:57:33 -0500

    Change use df as default to evalutate conditions

--- optimus/engines/base/dask/columns.py
+++ optimus/engines/base/dask/columns.py
@@ -662,10 +662,10 @@ class DaskBaseColumns(BaseColumns):
                     except ValueError:
                         pass
 
-                def func(pdf, _input,_value, _where):
+                def func(df, _input,_value, _where):
                     _value = eval(_value)
                     _where = eval(_where)
-                    return pdf[_input].where(~_where, _value)
+                    return df[_input].where(~_where, _value)
 
                 df = df.map_partitions(func, _input=input_col, _value=value, _where=where, meta=dtype)
                 # df[output_col] = df[input_col].where(~(where), value, meta=int)
