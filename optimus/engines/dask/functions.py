5e0b0d529bd6c6d677fabb86f45a906855f84a49
Argenis Leon <argenisleon@gmail.com>
2020-07-02 15:33:42 -0500

    Clean up imports

--- optimus/engines/dask/functions.py
+++ optimus/engines/dask/functions.py
@@ -1,16 +1,8 @@
 # This functions must handle one or multiple columns
 # Must return None if the data type can not be handle
-import dask
 from dask import delayed
 from dask.array import stats
 from dask.dataframe.core import DataFrame
-import numpy as np
-from optimus.helpers.check import is_column_a
-from optimus.helpers.columns import parse_columns
-from optimus.helpers.converter import format_dict
-from optimus.helpers.core import val_to_list
-from optimus.helpers.raiseit import RaiseIt
-from fast_histogram import histogram1d
 
 
 def functions(self):
@@ -31,7 +23,6 @@ def functions(self):
 
         @staticmethod
         def skewness(df, columns, args):
-
             f = {col_name: float(stats.skew(df[col_name])) for col_name in columns}
 
             @delayed
@@ -39,8 +30,6 @@ def functions(self):
                 return {"skewness": _f}
 
             return _skewness(f)
-
-
 
     return Functions()
 
