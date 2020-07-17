fbb68d9ca396e47a0c38cce780df04235fe2cf44
Argenis Leon <argenisleon@gmail.com>
2020-07-17 01:53:17 -0500

    Add function information for code suggestion

--- optimus/expressions.py
+++ optimus/expressions.py
@@ -52,7 +52,7 @@ op_functions = {
             },
         ],
         "example": "ABS(-2)",
-        "text": "ABS",
+        "text": "EXP",
     },
     "LOG": {
         "description": "Returns the logarithm of a number with respect to a base.",
@@ -64,47 +64,411 @@ op_functions = {
             },
         ],
         "example": "LOG(128, 2)",
-        "text": "ABS",
+        "text": "LOG",
     },
-    "LN": "LN function description",
-    "POW": "POW function description",
-    "CEILING": "CEILING function description",
-    "SQRT": "SQRT function description",
-    "FLOOR": "FLOOR function description",
-    "TRUNC": "TRUNC function description",
-    "RADIANS": "RADIANS function description",  # Trigonometric Functions
-    "DEGREES": "DEGREES function description",
-    "SIN": "SIN function description",
-    "COS": "COS function description",
-    "TAN": "TAN function description",
-    "ASIN": "ASIN function description",
-    "ACOS": "ACOS function description",
-    "ATAN": "ATAN function description",
-    "SINH": "SINH function description",
-    "ASINH": "ASINH function description",
-    "COSH": "COSH function description",
-    "TANH": "TANH function description",
-    "ACOSH": "ACOSH function description",
-    "ATANH": "ATANH function description",
-    "UPPER": "ATANH function description",  # String Transformations
-    "LOWER": "ATANH function description",
-    "PROPER": "ATANH function description",
-    "TRIM": "ATANH function description",
+    "LN": {
+        "description": "Returns the logarithm of a number, base e (Euler's number).",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The value for which to calculate the logarithm, base e."
+            },
+        ],
+        "example": "LN(100)",
+        "text": "LN",
+    },
+    "POW": {
+        "description": "Returns a number raised to a power.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The number to raise to the exponent power."
+            },
+            {
+                "type": "number",
+                "name": "exponent",
+                "description": "The exponent to raise base to."
+            }
+        ],
+        "example": "POW(4, 0.5)",
+        "text": "POW",
+    },
+    "CEIL": {
+        "description": "Rounds a number up to the nearest integer multiple of specified significance factor.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The value to round up to the nearest integer multiple of factor."
+            },
+
+        ],
+        "example": "CEILING(23.25)",
+        "text": "CEIL",
+    },
+    "SQRT": {
+        "description": "Returns the positive square root of a positive number.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The number for which to calculate the positive square root."
+            },
+
+        ],
+        "example": "SQRT(9)",
+        "text": "SQRT",
+    },
+    "FLOOR": {
+        "description": "Rounds a number down to the nearest integer multiple of specified significance factor.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The value to round down to the nearest integer multiple of factor."
+            },
+
+        ],
+        "example": "FLOOR(23.25)",
+        "text": "FLOOR",
+    },
+    # "TRUNC": {
+    #     "description": "Truncates a number to a certain number of significant digits by omitting less significant digits.",
+    #     "parameters": [
+    #         {
+    #             "type": "series",
+    #             "name": "series",
+    #             "description": "The value to round down to the nearest integer multiple of factor."
+    #         },
+    #
+    #     ],
+    #     "example": "FLOOR(23.25)",
+    #     "text": "TRUNC",
+    # },
+    # "RADIANS": "RADIANS function description",  # Trigonometric Functions
+    # "DEGREES": "DEGREES function description",
+    "SIN": {
+        "description": "Returns the sin of an angle provided in radians.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The angle to find the sine of, in radians."
+            },
+
+        ],
+        "example": "SIN(3.14)",
+        "text": "SIN",
+    },
+    "COS": {
+        "description": "Returns the cosine of an angle provided in radians.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The angle to find the cosine of, in radians."
+            },
+
+        ],
+        "example": "COS(3.14)",
+        "text": "COS",
+    },
+    "TAN": {
+        "description": "Returns the tangent of an angle provided in radians.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The angle to find the tangent of, in radians."
+            },
+
+        ],
+        "example": "TAN(3.14)",
+        "text": "TAN",
+    },
+    "ASIN": {
+        "description": "Returns the inverse sine of a value, in radians.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The value for which to calculate the inverse sine. Must be between -1 and 1, inclusive."
+            },
+
+        ],
+        "example": "ASIN(0)",
+        "text": "ASIN",
+    },
+    "ACOS": {
+        "description": "Returns the inverse cosine of a value, in radians.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The value for which to calculate the inverse cosine. Must be between -1 and 1, inclusive."
+            },
+
+        ],
+        "example": "ACOS(0)",
+        "text": "ACOS",
+    },
+    "ATAN": {
+        "description": "Returns the inverse tangent of a value, in radians.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The value for which to calculate the inverse tangent."
+            },
+
+        ],
+        "example": "ATAN(0)",
+        "text": "ATAN",
+    },
+    "SINH": {
+        "description": "Returns the hyperbolic sine of any real number.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "Any real value to calculate the hyperbolic sine of."
+            },
+
+        ],
+        "example": "SINH(2)",
+        "text": "SINH",
+    },
+    "COSH": {
+        "description": "Returns the hyperbolic cosine of any real number.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "Any real value to calculate the hyperbolic cosine of."
+            },
+
+        ],
+        "example": "COSH(0.48)",
+        "text": "COSH",
+    },
+
+    "TANH": {
+        "description": "Returns the hyperbolic tangent of any real number.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "Any real value to calculate the hyperbolic tangent of."
+            },
+
+        ],
+        "example": "TANH(1)",
+        "text": "TANH",
+    },
+    "ASINH": {
+        "description": "Returns the hyperbolic tangent of any real number.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The value for which to calculate the inverse hyperbolic sine."
+            },
+
+        ],
+        "example": "ASINH(0.9)",
+        "text": "ASINH",
+    },
+
+    "ACOSH": {
+        "description": "Returns the inverse hyperbolic cosine of a number.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The value for which to calculate the inverse hyperbolic cosine. Must be greater than or equal to 1."
+            },
+
+        ],
+        "example": "ACOSH(2)",
+        "text": "ACOSH",
+    },
+    "ATANH": {
+        "description": "Returns the inverse hyperbolic tangent of a number.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The value for which to calculate the inverse hyperbolic tangent. Must be between -1 and 1, exclusive."
+            },
+
+        ],
+        "example": "ATANH(0.9)",
+        "text": "ATANH",
+    },
+    "UPPER": {  # String Transformations
+        "description": "Returns the inverse hyperbolic tangent of a number.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "Converts a specified string to uppercase."
+            },
+
+        ],
+        "example": "UPPER('lorem ipsum')",
+        "text": "UPPER",
+    },
+    "LOWER": {
+        "description": "Converts a specified string to lowercase.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The string to convert to lowercase."
+            },
+
+        ],
+        "example": "LOWER('LOREM IPSUM')",
+        "text": "LOWER",
+    },
+    "PROPER": {
+        "description": "Capitalizes each word in a specified string.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The text which will be returned with the first letter of each word in uppercase and all other letters in lowercase."
+            },
+
+        ],
+        "example": "PROPER('optimus prime')",
+        "text": "PROPER",
+    },
+    "TRIM": {
+        "description": "Removes leading, trailing, and repeated spaces in text.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The text or reference to a cell containing text to be trimmed."
+            },
+
+        ],
+        "example": "TRIM('optimus prime')",
+        "text": "TRIM",
+    },
     "REMOVE": "ATANH function description",
-    "LEN": "ATANH function description",
-    "FIND": "ATANH function description",
+    "LEN": {
+        "description": "Returns the length of a string.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The string whose length will be returned."
+            },
+
+        ],
+        "example": "LEN('optimus prime')",
+        "text": "LEN",
+    },
+    "FIND": {
+        "description": "Returns the position at which a string is first found.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The string to look for."
+            },
+            {
+                "type": "string",
+                "name": "text_to_search",
+                "description": "The text to search for the first occurrence of search_for."
+            }
+
+        ],
+        "example": "LEN('optimus prime')",
+        "text": "LEN",
+    },
     "RFIND": "ATANH function description",
     "LEFT": "ATANH function description",
     "RIGHT": "ATANH function description",
     "STARTS_WITH": "ATANH function description",
     "ENDS_WITH": "ATANH function description",
     "EXACT": "ATANH function description",
-    "YEAR": "ATANH function description",  # Date Functions
-    "MONTH": "ATANH function description",
-    "DAY": "ATANH function description",
-    "HOUR": "ATANH function description",
-    "MINUTE": "ATANH function description",
-    "SECOND": "ATANH function description",
+    "YEAR": {                                   # Date Functions
+        "description": "Returns the year specified by a given date.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The date from which to extract the year."
+            },
+        ],
+        "example": "YEAR('7/20/1969')",
+        "text": "YEAR",
+    },
+    "MONTH": {
+        "description": "Returns the month of the year a specific date falls in, in numeric format.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The date from which to extract the month."
+            },
+        ],
+        "example": "MONTH('7/20/1969')",
+        "text": "MONTH",
+    },
+    "DAY": {
+        "description": "Returns the day of the month that a specific date falls on, in numeric format.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The date from which to extract the day."
+            },
+        ],
+        "example": 'DAY("7/20/1969")',
+        "text": "DAY",
+    },
+    "HOUR": {
+        "description": "Returns the hour component of a specific time, in numeric format.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The time from which to calculate the hour value."
+            },
+        ],
+        "example": 'DAY("7/20/1969")',
+        "text": "DAY",
+    },
+    "MINUTE": {
+        "description": "Returns the minute component of a specific time, in numeric format.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The time from which to calculate the minute value."
+            },
+        ],
+        "example": 'MINUTE("11:40:59 AM")',
+        "text": "MINUTE",
+    },
+    "SECOND": {
+        "description": "Returns the second component of a specific time, in numeric format.",
+        "parameters": [
+            {
+                "type": "series",
+                "name": "series",
+                "description": "The time from which to calculate the second value"
+            },
+        ],
+        "example": 'SECOND("11:40:59 AM")',
+        "text": "SECOND",
+    },
 
 }
 unary_operators = {
