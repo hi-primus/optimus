600816ba7aec4ed395d3658360ce9baf74062be3
Argenis Leon <argenisleon@gmail.com>
2020-10-10 15:03:57 -0500

    Fix functions not able to process numbers.

--- optimus/engines/functions.py
+++ optimus/engines/functions.py
@@ -1,5 +1,8 @@
 # Aggregations
+import math
+import numpy as np
 from optimus.engines.base.functions import Functions
+from optimus.infer import is_numeric
 
 
 def min(series):
@@ -51,10 +54,14 @@ def abs(series):
 
 
 def exp(series):
+    if is_numeric(series):
+        return math.exp(series)
     return series.functions.exp()
 
 
 def sqrt(series):
+    if is_numeric(series):
+        return math.sqrt(series)
     return series.functions.sqrt()
 
 
@@ -67,10 +74,14 @@ def pow(series, other):
 
 
 def floor(series):
+    if is_numeric(series):
+        return math.floor(series)
     return series.functions.floor()
 
 
 def ceil(series):
+    if is_numeric(series):
+        return math.ceil(series)
     return series.functions.ceil()
 
 
@@ -83,6 +94,8 @@ def range(series):
 
 
 def radians(series):
+    if is_numeric(series):
+        return math.radians(series)
     return series.functions.radian()
 
 
@@ -91,59 +104,87 @@ def degrees(series):
 
 
 def ln(series):
+    if is_numeric(series):
+        return np.log(series)
     return series.functions.ln()
 
 
 def log(series):
+    if is_numeric(series):
+        return np.log10(series)
     return series.functions.log()
 
 
 # Trigonometrics
 def sin(series):
+    if is_numeric(series):
+        return np.sin(series)
     return series.functions.sin()
 
 
 def cos(series):
+    if is_numeric(series):
+        return np.cos(series)
     return series.functions.cos()
 
 
 def tan(series):
+    if is_numeric(series):
+        return np.tan(series)
     return series.functions.tan()
 
 
 def asin(series):
+    if is_numeric(series):
+        return np.arcsin(series)
     return series.functions.asin()
 
 
 def acos(series):
+    if is_numeric(series):
+        return np.arccos(series)
     return series.functions.acos()
 
 
 def atan(series):
+    if is_numeric(series):
+        return np.arctan(series)
     return series.functions.atan()
 
 
 def sinh(series):
+    if is_numeric(series):
+        return np.sinh(series)
     return series.functions.sinh()
 
 
 def cosh(series):
+    if is_numeric(series):
+        return np.cosh(series)
     return series.functions.cosh()
 
 
 def tanh(series):
+    if is_numeric(series):
+        return np.tanh(series)
     return series.functions.tanh()
 
 
 def asinh(series):
+    if is_numeric(series):
+        return np.arcsinh(series)
     return series.functions.asinh()
 
 
 def acosh(series):
+    if is_numeric(series):
+        return np.arccosh(series)
     return series.functions.acosh()
 
 
 def atanh(series):
+    if is_numeric(series):
+        return np.arctanh(series)
     return series.functions.atanh()
 
 
