4a4e258e9246e7e6f727934e0eff66d50a9fff71
Argenis Leon <argenisleon@gmail.com>
2020-11-02 07:23:45 -0600

    Add coiled support

--- optimus/engines/dask/engine.py
+++ optimus/engines/dask/engine.py
@@ -1,7 +1,9 @@
+import dask
 from dask import dataframe as dd
 from dask.distributed import Client
-from optimus.engines.base.create import Create
+
 from optimus.bumblebee import Comm
+from optimus.engines.base.create import Create
 from optimus.engines.base.engine import BaseEngine
 from optimus.engines.dask.dask import Dask
 from optimus.engines.dask.io.load import Load
@@ -18,8 +20,7 @@ class DaskEngine(BaseEngine):
 
     # Using procces or threads https://stackoverflow.com/questions/51099685/best-practices-in-setting-number-of-dask-workers
     def __init__(self, session=None, address=None, n_workers=1, threads_per_worker=None, processes=False,
-                 memory_limit='4GB',
-                 verbose=False, comm=None, *args, **kwargs):
+                 memory_limit='4GB', verbose=False, comm=None, coiled_token=None, coiled_gpu=False, *args, **kwargs):
 
         if comm is True:
             Comm.instance = Comm()
@@ -35,11 +36,40 @@ class DaskEngine(BaseEngine):
         self.create = Create(dd)
         self.load = Load()
         self.verbose(verbose)
-        if address:
+
+        if coiled_token:
+            import coiled
+            dask.config.set({"coiled.token": coiled_token})
+            try:
+                coiled.Cloud()
+            except Exception as error:
+                raise error
+
+            cluster = coiled.Cluster(name="temp",
+                                     n_workers=n_workers,
+                                     # worker_memory=15,
+                                     worker_options={
+                                         "nthreads": threads_per_worker,
+                                         "memory_limit": memory_limit,
+                                     },
+                                     software={
+                                         coiled_gpu if "optimus/gpu" else "optimus/default"
+                                     },
+                                     )
+
+            Dask.instance = Client(cluster)
+
+            def install():
+                from optimus import Optimus
+                Optimus = Optimus
+                pass
+
+            Dask.instance.run(install)
+
+        elif address:
             Dask.instance = Client(address=address)
 
         elif session is None:
-            # print("PROCESS", processes)
             Dask.instance = Client(address=address, n_workers=n_workers, threads_per_worker=threads_per_worker,
                                    processes=processes,
                                    memory_limit=memory_limit, *args,
