bf35e1ff6b28e47da7fb93974ce42b55afcc92ed
Luis <luisaguirre027@gmail.com>
2020-11-20 10:06:47 -0400

    [FIX] n_rows support on pandas.load.csv

--- optimus/engines/pandas/io/load.py
+++ optimus/engines/pandas/io/load.py
@@ -51,7 +51,7 @@ class Load:
         return Load.csv(path, sep='\t', header=header, infer_schema=infer_schema, *args, **kwargs)
 
     @staticmethod
-    def csv(path, sep=",", header=True, infer_schema=True, encoding="UTF-8", null_value="None", quoting=3,
+    def csv(path, sep=",", header=True, infer_schema=True, encoding="UTF-8", n_rows=None, null_value="None", quoting=3,
             lineterminator="\n", error_bad_lines=False, cache=False, na_filter=False, *args, **kwargs):
         """
         Return a dataframe from a csv file. It is the same read.csv Spark function with some predefined
@@ -61,6 +61,7 @@ class Load:
         :param sep: usually delimiter mark are ',' or ';'.
         :param header: tell the function whether dataset has a header row. True default.
         :param infer_schema: infers the input schema automatically from data.
+        :param n_rows:
         :param null_value:
         :param charset:
         :param na_filter:
@@ -77,7 +78,7 @@ class Load:
             df_list = []
 
             for file_name, _ in local_file_names:
-                df = pd.read_csv(file_name, sep=sep, header=0 if header else -1, encoding=encoding,
+                df = pd.read_csv(file_name, sep=sep, header=0 if header else -1, encoding=encoding, nrows=n_rows,
                                  na_values=null_value,
                                  quoting=quoting, lineterminator=lineterminator, error_bad_lines=error_bad_lines,
                                  na_filter=na_filter, *args, **kwargs)
