7f9aa61a3ffe251c861d33895361d30c8a0303a4
Argenis Leon <argenisleon@gmail.com>
2021-01-04 01:16:53 -0600

    Functions now support numpy to process single  numerical data

--- optimus/functions.py
+++ optimus/functions.py
@@ -1,3 +1,6 @@
+from optimus.engines.base.odataframe import BaseDataFrame
+import numpy as np
+
 class Functions(type):
     """
     Handle the method name and execute the function corresponding to the dataframe class
@@ -9,11 +12,15 @@ class Functions(type):
         :return:
         """
 
-        def _func(df, *args):
+        def _func(df_or_value, *args):
             # Get the internal series in the optimus dataframe
-            _df = df.data[df.cols.names(0)[0]]
-            # Convert the frame to a dataframe and to a optimus Dataframe
-            return df.__class__(getattr(df.functions, func)(_df, *args).to_frame())
+            # print("df_or_value",type(df_or_value),df_or_value)
+            if isinstance(df_or_value, BaseDataFrame):
+                _df = df_or_value.data[df_or_value.cols.names(0)[0]]
+                # Convert the frame to a dataframe and to a optimus Dataframe
+                return df_or_value.__class__(getattr(df_or_value.functions, func)(_df, *args).to_frame())
+            else:
+                return getattr(np, func)(df_or_value, *args)
 
         return _func
 
