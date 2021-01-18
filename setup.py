0e3c878d1167596f521b60eac4d7d20ef616d6f3
Luis <luisaguirre027@gmail.com>
2021-01-18 11:03:33 -0400

    Extra requirements adjusted

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ import sys
 
 from setuptools import setup, find_packages
 
+from functools import reduce
+
+
 
 # from optimus.version import __version__
 
@@ -12,6 +15,15 @@ def get_version():
     with open('optimus/version.py') as version_file:
         return re.search(r"""__version__\s+=\s+(['"])(?P<version>.+?)\1""",
                          version_file.read()).group('version')
+
+
+if sys.version_info < (3, 6):
+    raise RuntimeError('This version requires Python 3.6+')  # pragma: no cover
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 
 # Requirements
@@ -32,26 +44,18 @@ else:
     with open('requirements.txt') as f:
         required = f.read().splitlines()
 
-if sys.version_info < (3, 6):
-    raise RuntimeError('This version requires Python 3.6+')  # pragma: no cover
+extras_requirements_keys = ['spark', 'ai', 'plotting', 'db']
+extras_requirements = {}
+for extra in extras_requirements_keys:
+    with open('requirements-'+extra+'.txt') as f:
+        extras_requirements[extra] = f.read().splitlines()
 
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
-lint_requires = [
-    'pep8',
-    'pyflakes'
-]
-
-tests_require = ['pytest', 'mock', 'nose']
-
+lint_requirements = ['pep8', 'pyflakes']
+test_requirements = ['pytest', 'mock', 'nose']
 dependency_links = []
-setup_requires = ['pytest-runner']
+setup_requirements = ['pytest-runner']
 if 'nosetests' in sys.argv[1:]:
-    setup_requires.append('nose')
+    setup_requirements.append('nose')
 
 setup(
     name='optimuspyspark',
@@ -59,21 +63,20 @@ setup(
     author='Favio Vazquez and Argenis Leon',
     author_email='argenisleon@gmail.com',
     url='https://github.com/ironmussa/Optimus/',
-    download_url='https://github.com/ironmussa/Optimus/archive/2.2.31.tar.gz',
-    description=('Optimus is the missing framework for cleaning and pre-processing data in a distributed fashion with '
-                 'pyspark.'),
+    description=('Optimus is the missing framework for cleaning and pre-processing data in a distributed fashion.'),
     long_description=readme(),
     long_description_content_type='text/markdown',
     license='APACHE',
     packages=find_packages(),
     install_requires=required,
-    tests_require=tests_require,
-    setup_requires=setup_requires,
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
     extras_require={
-        'test': tests_require,
-        'all': required + tests_require,
-        'docs': ['sphinx'] + tests_require,
-        'lint': lint_requires
+        'test': test_requirements,
+        'all': test_requirements + reduce(lambda x, key: x + extras_requirements[key], extras_requirements, []),
+        'docs': ['sphinx'] + test_requirements,
+        'lint': lint_requirements,
+        **extras_requirements
     },
     dependency_links=dependency_links,
     test_suite='nose.collector',
