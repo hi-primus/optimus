1507648c92b04e2c925aa736145397c752b39487
Luis <luisaguirre027@gmail.com>
2021-02-19 21:27:30 -0400

    Implement Dask-cuDF dataframe

--- optimus/engines/dask_cudf/dataframe.py
+++ optimus/engines/dask_cudf/dataframe.py
@@ -1,5 +1,9 @@
 from optimus.engines.base.dask.dataframe import DaskBaseDataFrame
 from optimus.engines.dask_cudf.io.save import Save
+from optimus.engines.buffer import _set_buffer, _buffer_windows
+from optimus.engines.cudf.dataframe import CUDFDataFrame
+from optimus.engines.pandas.dataframe import PandasDataFrame
+from optimus.helpers.constants import BUFFER_SIZE
 
 
 class DaskCUDFDataFrame(DaskBaseDataFrame):
@@ -26,6 +30,22 @@ class DaskCUDFDataFrame(DaskBaseDataFrame):
     def save(self):
         return Save(self)
 
+    @property
+    def mask(self):
+        from optimus.engines.base.mask import Mask
+        return Mask(self)
+
+    @property
+    def constants(self):
+        from optimus.engines.base.dask.constants import constants
+        return constants(self)
+
+    def set_buffer(self, columns="*", n=BUFFER_SIZE):
+        return _set_buffer(self, columns=columns, n=n)
+
+    def buffer_window(self, columns=None, lower_bound=None, upper_bound=None, n=BUFFER_SIZE):
+        return _buffer_windows(self, columns=columns, lower_bound=lower_bound, upper_bound=upper_bound, n=n)
+
     @staticmethod
     def pivot(index, column, values):
         pass
@@ -48,6 +68,12 @@ class DaskCUDFDataFrame(DaskBaseDataFrame):
 
     def to_pandas(self):
         return self.data.compute().to_pandas()
+
+    def to_optimus_pandas(self):
+        return PandasDataFrame(self.root.to_pandas())
+
+    def to_optimus_cudf(self):
+        return CUDFDataFrame(self.root.to_pandas())
 
 
     def to_dict(self, orient="records", limit=None):
