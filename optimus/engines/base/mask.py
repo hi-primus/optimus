001f48c8abc59a78ce2e5794ff5aec6706b86d86
Luis <luisaguirre027@gmail.com>
2021-02-22 14:57:19 -0400

    Fix find, nulls, duplicated and empty functions
    Implemented contains

--- optimus/engines/base/mask.py
+++ optimus/engines/base/mask.py
@@ -1,5 +1,5 @@
 from optimus.helpers.core import val_to_list
-from optimus.infer import Infer
+from optimus.infer import Infer, is_str
 
 
 class Mask:
@@ -74,29 +74,31 @@ class Mask:
     def ends_with(self, col_name, value):
         mask = self.root.data[col_name].str.endswith(value, na=False)
         return self.root.new(mask.to_frame())
+    
+    def contains(self, col_name, value):
+        mask = self.root.data[col_name].str.contains(value, na=False)
+        return self.root.new(mask.to_frame())
 
-    def find(self, value):
-        """
+    def find(self, col_name, value):
+        if is_str(value):
+            mask = self.root.data[col_name].astype(str).str.match(value, na=False)
+        else:
+            mask = dfd[col_name] == value
+        return self.root.new(mask.to_frame())
 
-        :param value: Regex or string
-        :return:
-        """
-
-        pass
-
-    def nulls(self, col_name, how="any"):
+    def nulls(self, columns, how="any"):
         """
         Find the rows that have null values
         :param how:
-        :param col_name:
+        :param columns:
         :return:
         """
 
         dfd = self.root.data
 
-        if col_name is not None:
-            columns = val_to_list(col_name)
-            subset_df = dfd[columns]
+        if columns is not None:
+            subset = val_to_list(columns)
+            subset_df = dfd[subset]
         else:
             subset_df = dfd
 
@@ -107,7 +109,7 @@ class Mask:
 
         return self.root.new(mask)
 
-    def duplicated(self, keep="first", columns=None):
+    def duplicated(self, columns, keep="first"):
         """
         Find the rows that have duplicated values
 
@@ -117,6 +119,7 @@ class Mask:
         """
 
         dfd = self.root.data
+
         if columns is not None:
             subset = val_to_list(columns)
             subset_df = dfd[subset]
@@ -134,5 +137,5 @@ class Mask:
         :param col_name:
         :return:
         """
-        mask = self.root.data[col_name] = ""
+        mask = self.root.data[col_name] == ""
         return self.root.new(mask)
