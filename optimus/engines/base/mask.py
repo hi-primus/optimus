bc3a39b2c2e666676575635037fd40b5b2c8c6bd
Argenis Leon <argenisleon@gmail.com>
2021-02-26 08:08:30 -0600

    Refactoring Mask implementation

--- optimus/engines/base/mask.py
+++ optimus/engines/base/mask.py
@@ -1,8 +1,12 @@
+from abc import abstractmethod, ABC
+
+from optimus.helpers.columns import parse_columns
+from optimus.helpers.constants import ProfilerDataTypes
 from optimus.helpers.core import val_to_list
 from optimus.infer import Infer, is_str
 
 
-class Mask:
+class Mask(ABC):
     def __init__(self, root):
         self.root = root
 
@@ -88,6 +92,10 @@ class Mask:
             mask = dfd[col_name] == value
         return self.root.new(mask.to_frame())
 
+    @abstractmethod
+    def integer(self,col_name):
+        pass
+
     def nulls(self, columns, how="any"):
         """
         Find the rows that have null values
@@ -95,11 +103,11 @@ class Mask:
         :param columns:
         :return:
         """
-
+        df = self.root
         dfd = self.root.data
 
         if columns is not None:
-            subset = val_to_list(columns)
+            subset = parse_columns(df, columns)
             subset_df = dfd[subset]
         else:
             subset_df = dfd
@@ -141,3 +149,58 @@ class Mask:
         """
         mask = self.root.data[col_name] == ""
         return self.root.new(mask)
+
+    def string(self, col_name="*"):
+        return ~self.root.mask.nulls(col_name)
+
+    def email(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.EMAIL.value])
+
+    def ip(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.IP.value])
+
+    def url(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.URL.value])
+
+    def gender(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.GENDER.value])
+
+    def boolean(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.BOOLEAN.value])
+
+    def zip_code(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.ZIP_CODE.value])
+
+    def credit_card_number(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.CREDIT_CARD_NUMBER.value])
+
+    def date(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.DATE.value])
+
+    def object(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.OBJECT.value])
+
+    def array(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.ARRAY.value])
+
+    def phone_number(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.PHONE_NUMBER.value])
+
+    def social_security_number(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.SOCIAL_SECURITY_NUMBER.value])
+
+    def http_code(self, col_name="*"):
+        return self.root[col_name].cols.to_string().cols.match(col_name, Infer.ProfilerDataTypesFunctions[
+            ProfilerDataTypes.HTTP_CODE.value])
