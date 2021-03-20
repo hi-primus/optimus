e86d29779956d624d3414423eb80e36240ac8bc0
Argenis Leon <argenisleon@gmail.com>
2021-03-20 12:17:44 -0600

    Add numeric functions to pandas and dask

--- optimus/engines/dask/mask.py
+++ optimus/engines/dask/mask.py
@@ -2,7 +2,7 @@ import fastnumbers
 import numpy as np
 import pandas as pd
 
-from optimus.engines.base.commons.functions import is_integer, is_float
+from optimus.engines.base.commons.functions import is_integer, is_float, is_numeric
 from optimus.engines.base.mask import Mask
 
 
@@ -13,5 +13,8 @@ class DaskMask(Mask):
 
     def float(self, col_name="*"):
         return self.root.cols.apply(col_name, is_float, mode="partitioned")
+
+    def numeric(self, col_name="*"):
+        return self.root.cols.apply(col_name, is_numeric, mode="partitioned")
 
 
--- optimus/engines/pandas/mask.py
+++ optimus/engines/pandas/mask.py
@@ -2,7 +2,7 @@ import fastnumbers
 import numpy as np
 import pandas as pd
 
-from optimus.engines.base.commons.functions import is_integer, is_float
+from optimus.engines.base.commons.functions import is_integer, is_float, is_numeric
 from optimus.engines.base.mask import Mask
 
 
@@ -12,7 +12,8 @@ class PandasMask(Mask):
         return self.root.cols.apply(col_name, is_integer)
 
     def float(self, col_name="*"):
-
         return self.root.cols.apply(col_name, is_float)
 
+    def numeric(self, col_name="*"):
+        return self.root.cols.apply(col_name, is_numeric)
 
