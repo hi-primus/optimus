43402af60db3dca1e69545441c8316edb0071658
Argenis Leon <argenisleon@gmail.com>
2021-04-06 21:50:37 -0500

    Remove unused imports

--- optimus/engines/dask/mask.py
+++ optimus/engines/dask/mask.py
@@ -1,7 +1,3 @@
-import fastnumbers
-import numpy as np
-import pandas as pd
-
 from optimus.engines.base.commons.functions import is_integer, is_float, is_numeric
 from optimus.engines.base.mask import Mask
 
@@ -16,5 +12,3 @@ class DaskMask(Mask):
 
     def numeric(self, col_name="*"):
         return self.root.cols.apply(col_name, is_numeric, mode="partitioned")
-
-
