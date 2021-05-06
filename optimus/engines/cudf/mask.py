7d0a369a55399e1fb1b12ffb407e6d89f5add2e3
Argenis Leon <argenisleon@gmail.com>
2021-05-05 20:08:50 -0500

    Add missing numeric function

--- optimus/engines/cudf/mask.py
+++ optimus/engines/cudf/mask.py
@@ -2,11 +2,14 @@ import fastnumbers
 import numpy as np
 import pandas as pd
 
-from optimus.engines.base.commons.functions import is_integer, is_float, is_integer_cudf, is_float_cudf
+from optimus.engines.base.commons.functions import is_integer, is_float, is_integer_cudf, is_float_cudf, is_numeric_cudf
 from optimus.engines.base.mask import Mask
 
 
 class CUDFMask(Mask):
+
+    def numeric(self, col_name):
+        return self.root.cols.apply(col_name, is_numeric_cudf)
 
     def integer(self, col_name="*"):
         return self.root.cols.apply(col_name, is_integer_cudf)
