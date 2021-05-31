12b2244084bac85aaa727e7f864434ea7fe5c6df
Argenis Leon <argenisleon@gmail.com>
2021-05-31 14:01:44 -0500

    Implement hist in dask using native da.histogram

--- optimus/engines/dask/columns.py
+++ optimus/engines/dask/columns.py
@@ -1,7 +1,12 @@
+import dask.array as da
 from dask_ml import preprocessing
 
 from optimus.engines.base.commons.functions import impute, string_to_index, index_to_string
 from optimus.engines.base.dask.columns import DaskBaseColumns
+from optimus.helpers.columns import parse_columns
+import dask
+
+from optimus.profiler.constants import MAX_BUCKETS
 
 
 class Cols(DaskBaseColumns):
@@ -22,24 +27,23 @@ class Cols(DaskBaseColumns):
         le = preprocessing.LabelEncoder()
         return index_to_string(self, input_cols, output_cols, le)
 
-    # def hist(self, columns="*", buckets=20, compute=True):
-    #     df = self.df
-    #     columns = parse_columns(df, columns)
-    #
-    #     result = {}
-    #     for col_name in columns:
-    #
-    #         df_numeric = df[col_name].to_float()
-    #
-    #         if len(df_numeric) > 0:
-    #             _count, _bins = da.histogram(df["id"].astype(int), bins=buckets, range=[1, 19])
-    #             # _count, _bins = cp.histogram(df_numeric, buckets)
-    #             result[col_name] = [
-    #                 {"lower": float(_bins[i]), "upper": float(_bins[i + 1]), "count": int(_count[i])}
-    #                 for i in range(buckets)]
-    #
-    #     return {"hist":result}
+    def hist(self, columns="*", buckets=MAX_BUCKETS, compute=True):
+        df = self.root
+        columns = parse_columns(df, columns)
 
-    def impute(self, input_cols, data_type="continuous", strategy="mean", fill_value=None,output_cols=None):
+        result = {}
+        for col_name in columns:
+            # dfd = df[col_name].cols.to_float().data
+            dfd = df.data[col_name].astype("float")
+
+            if len(dfd) > 0:
+                _count, _bins = dask.compute(da.histogram(dfd, bins=buckets, range=[dfd.min(), dfd.max()]))[0]
+                result[col_name] = [
+                    {"lower": float(_bins[i]), "upper": float(_bins[i + 1]), "count": _count[i]}
+                    for i in range(buckets)]
+
+        return {"hist": result}
+
+    def impute(self, input_cols, data_type="continuous", strategy="mean", fill_value=None, output_cols=None):
         df = self.root
         return impute(df, input_cols, data_type=data_type, strategy=strategy, output_cols=None)
