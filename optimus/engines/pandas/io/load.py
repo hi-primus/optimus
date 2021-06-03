1a627a9756b072845fe6ed9f680f4e9be24e582e
Luis <luisaguirre027@gmail.com>
2021-06-02 20:29:47 -0400

    Add n_rows argument support on load.json on Pandas

--- optimus/engines/pandas/io/load.py
+++ optimus/engines/pandas/io/load.py
@@ -23,7 +23,7 @@ class Load(BaseLoad):
         self.op = op
 
     @staticmethod
-    def json(path, multiline=False, *args, **kwargs):
+    def json(path, multiline=False, n_rows=-1, *args, **kwargs):
         """
         Return a dataframe from a json file.
         :param path: path or location of the file.
@@ -39,7 +39,7 @@ class Load(BaseLoad):
             df_list = []
 
             for file_name, j in local_file_names:
-                df = pd.read_json(file_name, lines=multiline, *args, **kwargs)
+                df = pd.read_json(file_name, lines=multiline, nrows=n_rows, *args, **kwargs)
                 df_list.append(df)
 
             df = pd.concat(df_list, axis=0, ignore_index=True)
