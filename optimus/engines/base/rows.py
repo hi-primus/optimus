e201480e899701ede0f504547051a71a13766388
Luis <luisaguirre027@gmail.com>
2021-07-06 17:52:19 -0400

    Fix mask functions on rows

--- optimus/engines/base/rows.py
+++ optimus/engines/base/rows.py
@@ -8,7 +8,7 @@ from optimus.engines.base.meta import Meta
 # This implementation works for Spark, Dask, dask_cudf
 from optimus.helpers.columns import parse_columns
 from optimus.helpers.constants import Actions
-from optimus.helpers.core import one_list_to_val
+from optimus.helpers.core import one_list_to_val, val_to_list
 from optimus.helpers.raiseit import RaiseIt
 from optimus.infer import is_str, is_list_of_str_or_int
 
@@ -54,72 +54,6 @@ class BaseRows(ABC):
     @abstractmethod
     def append(self, dfs: DataFrameTypeList, cols_map) -> DataFrameType:
         pass
-
-    def greater_than(self, input_col, value) -> DataFrameType:
-        df = self.root
-        return df[df.mask.greater_than(input_col, value)]
-        # dfd = self.root.data
-        #
-        # return self.root.new(dfd[self.root.mask.greater_than(input_col, value).get_series()])
-
-    def greater_than_equal(self, input_col, value) -> DataFrameType:
-
-        dfd = self.root.data
-        return self.root.new(dfd[self.root.greater_than_equal(input_col, value)])
-
-    def less_than(self, input_col, value) -> DataFrameType:
-
-        dfd = self.root.data
-        return self.root.new(dfd[self.root.less_than(input_col, value)])
-
-    def less_than_equal(self, input_col, value) -> DataFrameType:
-
-        dfd = self.root.data
-        return self.root.new(dfd[self.root.less_than_equal(input_col, value)])
-
-    def equal(self, input_col, value) -> DataFrameType:
-        dfd = self.root.data
-        return self.root.new(dfd[self.root.mask.is_equal(input_col, value)])
-
-    def not_equal(self, input_col, value) -> DataFrameType:
-
-        dfd = self.root.data
-        return self.root.new(dfd[self.root.mask.not_equal(input_col, value)])
-
-    def missing(self, input_col) -> DataFrameType:
-        """
-        Return missing values
-        :param input_col:
-        :return:
-        """
-        dfd = self.root.data
-        return self.root.new(dfd[self.root.mask.missing(input_col)])
-
-    def mismatch(self, input_col="*", dtype=None) -> DataFrameType:
-        """
-        Return mismatches values
-        :param input_col:
-        :param dtype:
-        :return:
-        """
-        df = self.root
-        return df[df.mask.mismatch(input_col, dtype)]
-
-        # dfd = self.root.data
-        # return self.root.new(dfd[self.root.mask.mismatch(input_col, dtype).get_series()])
-
-    def match(self, col_name="*", dtype=None) -> DataFrameType:
-        """
-        Return Match values
-        :param col_name:
-        :param dtype:
-        :return:
-        """
-        df = self.root
-        return df[df.mask.match(col_name, dtype)]
-
-        # dfd = self.root.data
-        # return self.root.new(dfd[self.root.mask.match(col_name, dtype).get_series()])
 
     def apply(self, func, args=None, output_cols=None) -> DataFrameType:
         """
@@ -297,41 +231,51 @@ class BaseRows(ABC):
         meta = Meta.action(df.meta, Actions.DROP_ROW.value, df.cols.names())
         return self.root.new(dfd, meta=meta)
 
-    @staticmethod
-    @abstractmethod
-    def between(columns, lower_bound=None, upper_bound=None, invert=False, equal=False,
-                bounds=None) -> DataFrameType:
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def drop_by_dtypes(input_cols, data_type=None) -> DataFrameType:
-        pass
-
-    def drop_na(self, subset=None, how="any", *args, **kwargs) -> DataFrameType:
+    def between_index(self, cols="*", lower_bound=None, upper_bound=None):
         """
-        Removes rows with null values. You can choose to drop the row if 'all' values are nulls or if
-        'any' of the values is null.
-        :param subset:
-        :param how:
+
+        :param columns:
+        :param lower_bound:
+        :param upper_bound:
         :return:
         """
+        dfd = self.root.data
+        cols = val_to_list(parse_columns(dfd, cols))
+        return self.root.new(dfd[lower_bound: upper_bound][cols])
+
+    def between(self, cols="*", lower_bound=None, upper_bound=None, equal=False, bounds=None, drop=False, how="any") -> DataFrameType:
+        
         df = self.root
-        subset = parse_columns(df, subset)
-        meta = Meta.action(df.meta, Actions.DROP_ROW.value, df.cols.names())
-        return self.root.new(df.data.dropna(how=how, subset=subset), meta=meta)
+        
+        if bounds is None:
+            bounds = [(lower_bound, upper_bound)]
 
-    @staticmethod
-    @abstractmethod
-    def drop_duplicates(input_cols=None) -> DataFrameType:
-        """
-        Drop duplicates values in a dataframe
-        :param input_cols: List of columns to make the comparison, this only  will consider this subset of columns,
-        :return: Return a new DataFrame with duplicate rows removed
-        :param input_cols:
-        :return:
-        """
-        pass
+        mask = None
+
+        for bound in bounds:
+            if equal:
+                _mask = (df[cols] >= bound[0]) & (df[cols] <= bound[1])
+            else:
+                _mask = (df[cols] > bound[0]) & (df[cols] < bound[1])
+            
+            if mask is None:
+                mask = _mask
+            else:
+                mask = mask | _mask
+
+        if how=="any":
+            mask = mask.mask.any()
+        elif how=="all":
+            mask = mask.mask.all()
+        else:
+            RaiseIt.value_error(how, ["any", "all"])
+
+        if drop:
+            mask = ~mask
+     
+        df = df.rows.select(mask)
+        
+        return df
 
     def limit(self, count=10) -> DataFrameType:
         """
@@ -340,15 +284,6 @@ class BaseRows(ABC):
         :return:
         """
         return self.root.new(self.root.data[:count])
-
-    def is_in(self, input_cols, values, output_cols=None) -> DataFrameType:
-
-        def _is_in(value, *args):
-            _values = args
-            return value.isin(_values)
-
-        df = self.root
-        return df.cols.apply(input_cols, func=_is_in, args=(values,), output_cols=output_cols)
 
     @staticmethod
     @abstractmethod
@@ -361,3 +296,131 @@ class BaseRows(ABC):
         :return:
         """
         return self.root.rows.count()
+
+    def _mask(self, cols, method, drop=False, how="any", *args, **kwargs) -> DataFrameType:
+
+        df = self.root
+        mask = getattr(df.mask, method)(cols, *args, **kwargs)
+
+        if how=="any":
+            mask = mask.mask.any()
+        elif how=="all":
+            mask = mask.mask.all()
+        else:
+            RaiseIt.value_error(how, ["any", "all"])
+
+        if drop:
+            mask = ~mask
+
+        df = df.rows.select(mask)
+
+        return df
+
+    def greater_than_equal(self, cols="*", value=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="greater_than_equal", drop=drop, value=value, how=how)
+
+    def greater_than(self, cols="*", value=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="greater_than", drop=drop, value=value, how=how)
+
+    def greater_than_equal(self, cols="*", value=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="greater_than_equal", drop=drop, value=value, how=how)
+
+    def greater_than_equal(self, cols="*", value=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="greater_than_equal", drop=drop, value=value, how=how)
+
+    def equal(self, cols="*", value=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="equal", drop=drop, value=value, how=how)
+
+    def not_equal(self, cols="*", value=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="not_equal", drop=drop, value=value, how=how)
+
+    def missing(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="missing", drop=drop, how=how)
+
+    def null(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="null", drop=drop, how=how)
+
+    def none(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="none", drop=drop, how=how)
+
+    def nan(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="nan", drop=drop, how=how)
+
+    def empty(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="empty", drop=drop, how=how)
+
+    def duplicated(self, cols="*", keep="first", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="duplicated", drop=drop, keep=keep, how=how)
+
+    def mismatch(self, cols="*", dtype=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="mismatch", drop=drop, dtype=dtype, how=how)
+
+    def match(self, cols="*", regex=None, dtype=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="match", drop=drop, regex=regex, dtype=dtype, how=how)
+
+    def match_regex(self, cols="*", regex=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="match_regex", drop=drop, regex=regex, how=how)
+
+    def match_dtype(self, cols="*", dtype=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="match_dtype", drop=drop, dtype=dtype, how=how)
+
+    def value_in(self, cols="*", values=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="value_in", drop=drop, values=values, how=how)
+
+    def pattern(self, cols="*", pattern=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="pattern", drop=drop, pattern=pattern, how=how)
+
+    def starts_with(self, cols="*", value=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="starts_with", drop=drop, value=value, how=how)
+
+    def ends_with(self, cols="*", value=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="ends_with", drop=drop, value=value, how=how)
+
+    def contains(self, cols="*", value=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="contains", drop=drop, value=value, how=how)
+
+    def find(self, cols="*", value=None, drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="find", drop=drop, value=value, how=how)
+
+    def empty(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="empty", drop=drop, how=how)
+
+    def email(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="email", drop=drop, how=how)
+    
+    def ip(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="ip", drop=drop, how=how)
+    
+    def url(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="url", drop=drop, how=how)
+    
+    def gender(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="gender", drop=drop, how=how)
+    
+    def boolean(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="boolean", drop=drop, how=how)
+    
+    def zip_code(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="zip_code", drop=drop, how=how)
+    
+    def credit_card_number(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="credit_card_number", drop=drop, how=how)
+    
+    def datetime(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="datetime", drop=drop, how=how)
+    
+    def object(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="object", drop=drop, how=how)
+    
+    def array(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="array", drop=drop, how=how)
+    
+    def phone_number(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="phone_number", drop=drop, how=how)
+    
+    def social_security_number(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="social_security_number", drop=drop, how=how)
+    
+    def http_code(self, cols="*", drop=False, how="any") -> DataFrameType:
+        return self._mask(cols, method="http_code", drop=drop, how=how)
+    
