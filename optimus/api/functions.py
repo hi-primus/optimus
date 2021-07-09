6871a79295ff88081af1765e642f65273796c790
Luis <luisaguirre027@gmail.com>
2021-07-09 18:49:28 -0400

    Implement sessions in REST API

---
+++ optimus/api/functions.py
@@ -0,0 +1,73 @@
+from optimus.api.code import generate_code
+
+
+def get_optimus_features():
+    try:
+        import pyspark
+        spark_available = True
+    except:
+        spark_available = False
+
+    try:
+        import coiled
+        coiled_available = True
+    except:
+        coiled_available = False
+
+    try:
+        import dask
+        dask_available = True
+    except:
+        dask_available = False
+
+    try:
+        import cudf
+        import dask_cudf
+        rapids_available = True
+    except:
+        rapids_available = False
+
+    coiled_gpu_available = coiled_available
+
+    res = {
+        "coiled_available": coiled_available,
+        "coiled_gpu_available": coiled_gpu_available,
+        "spark_available": spark_available,
+        "dask_available": dask_available,
+        "rapids_available": rapids_available
+    }
+
+    try:
+        from optimus.expressions import reserved_words
+        res.update({'reserved_words': reserved_words})
+    except:
+        pass
+
+    return res
+
+def run_or_code(session, body, run=False):
+    
+    code, updated = generate_code(body, list(session.keys()))
+
+    res = {
+        "status": "ok", 
+        "code": code.split("\n")
+    }
+
+    if run:
+        exec("result = None", session)
+        try:
+            exec("from optimus import Optimus", session)
+            exec(code, session)
+        except Exception as error:
+            res.update({"error": str(error)})
+            res.update({"status": "error"})
+            
+        result = eval("result or None", session)
+        if result:
+            res.update({"result": result})
+
+    if updated and len(updated):
+        res.update({"updated": updated})
+
+    return res, session
--- run.py
+++ run.py
@@ -1,62 +1,108 @@
+from optimus.api.functions import get_optimus_features, run_or_code
 from flask import Flask
 from flask import request, render_template
-
-from optimus.api.code import generate_code
 
 import json
 
 app = Flask(__name__, template_folder="optimus/api/templates")
 
-session = {}
+optimus_features = None
+sessions = {}
 
-def _run_code(body, run=False):
+def _run_or_code(session_key, body, run=False):
 
-    global session
+    global sessions
+    print(type(body))
 
-    code, updated = generate_code(body, list(session.keys()))
+    session = sessions.get(session_key, {})
 
-    res = {
-        "status": "ok", 
-        "code": code.split("\n")
-    }
+    res, session = run_or_code(session, body, run)
 
-    if run:
-        exec("result = None", session)
+    if session_key != "default":
+        res.update({"session": session_key})
 
-        try:
-            exec("from optimus import Optimus", session)
-            exec(code, session)
-        except Exception as error:
-            res.update({"error": str(error)})
-            res.update({"status": "error"})
-            
-        result = eval("result or None", session)
-        if result:
-            res.update({"result": result})
-
-    if updated and len(updated):
-        res.update({"updated": updated})
+    sessions.update({session_key: session})
 
     return res
 
-@app.route('/run', methods=['POST'])
-def run():
+@app.route('/<session_key>/run', methods=['POST'])
+def run(session_key):
     body = json.loads(request.data)
-    res = _run_code(body, True)
+    res = _run_or_code(session_key, body, True)
     return json.dumps(res)
 
-@app.route('/code', methods=['POST'])
-def code():
+@app.route('/<session_key>/code', methods=['POST'])
+def code(session_key):
     body = json.loads(request.data)
-    res = _run_code(body, False)
+    res = _run_or_code(session_key, body, False)
     return json.dumps(res)
 
-@app.route('/initEngine', methods=['POST'])
-def initEngine():
+@app.route('/<session_key>/init-engine', methods=['POST'])
+def init_engine(session_key):
     body = json.loads(request.data)
     body["operation"] = "Optimus"
-    res = _run_code(body, False)
+    res = _run_or_code(session_key, body, False)
     return json.dumps(res)
+
+@app.route('/<session_key>', methods=['GET'])
+def session(session_key):
+    global sessions
+    
+    if session_key is None:
+        res = {"status": "error", "content": "Session not found in request", "code": 400}
+    else:
+        session = sessions.pop(session_key, None)
+        if session is None:
+            res = {"status": "error", "content": "Session not found", "code": 404}
+        else:
+            res = {"status": "ok", "content": "Session found", "code": 200}
+
+    return json.dumps(res)
+
+@app.route('/<session_key>', methods=['POST'])
+def create_session(session_key):
+    global sessions
+    
+    if session_key is None:
+        res = {"status": "error", "content": "Session not found in request", "code": 400}
+    else:
+        session = sessions.pop(session_key, None)
+        if session is None:
+            sessions.update({session: {}})
+            res = {"status": "ok", "content": "Session created", "code": 200}
+        else:
+            res = {"status": "ok", "content": "Session already created", "code": 200}
+
+    return json.dumps(res)
+
+@app.route('/<session_key>', methods=['DELETE'])
+def delete_session(session_key):
+    global sessions
+    body = json.loads(request.data)
+    session_key = body.pop("session", None)
+    
+    if session_key is None:
+        res = {"status": "error", "content": "Session not found in request", "code": 400}
+    else:
+        session = sessions.pop(session_key, None)
+        if session is None:
+            res = {"status": "error", "content": "Session not found", "code": 404}
+        else:
+            del sessions[session_key]
+            res = {"status": "ok", "content": "Session deleted", "code": 200}
+
+    return json.dumps(res)
+
+@app.route('/features', methods=['GET'])
+def features():
+    
+    global optimus_features
+
+    if optimus_features is None:
+        optimus_features = get_optimus_features()
+
+    return json.dumps(optimus_features)
+    
 
 @app.route('/', methods=['GET'])
 def playground():
