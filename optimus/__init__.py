bfd092e7e159ba3bb548a82cebb6d847ebcdab9f
Luis <luisaguirre027@gmail.com>
2021-07-14 09:56:37 -0400

    Fix version

--- optimus/__init__.py
+++ optimus/__init__.py
@@ -3,10 +3,11 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
 
 from .optimus import optimus as Optimus
+from optimus.version import __version__
 # Handle encoding problem
 # https://stackoverflow.com/questions/39662384/pyspark-unicodeencodeerror-ascii-codec-cant-encode-character
 
-# Preserve compatibility with <3.x brach
+# Preserve compatibility with <3.x branch
 os.environ["PYTHONIOENCODING"] = "utf8"
 
 
