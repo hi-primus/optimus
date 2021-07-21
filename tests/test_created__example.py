e5584b9d591cae9dceb5d02f51de1cbd852b847f
Luis <luisaguirre027@gmail.com>
2021-07-21 10:04:24 -0400

    Update created example test file

--- tests/test_created__example.py
+++ tests/test_created__example.py
@@ -4,11 +4,11 @@ Timestamp = lambda t: datetime.datetime.strptime(t,"%Y-%m-%d %H:%M:%S")
 nan = float("nan")
 inf = float("inf")
 from optimus.helpers.json import json_encoding
-from optimus.helpers.functions import deep_sort
+from optimus.helpers.functions import deep_sort, df_dicts_equal
 
 class TestExamplePandas(TestBase):
     config = {'engine': 'pandas'}
-    dict = {('name', 'object'): ['Optimus', 'Bumblebee', 'Eject'], ('age (M)', 'int'): [5, 5, 5]}
+    dict = {('name', 'object'): ['Optimus', 'Bumblebee', 'Eject'], ('age (M)', 'int64'): [5, 5, 5]}
     maxDiff = None
     
     def test_cols_upper_multiple(self):
@@ -21,8 +21,8 @@ class TestExamplePandas(TestBase):
     def test_cols_upper_single(self):
         df = self.df
         result = df.cols.upper(cols=['name'])
-        expected = self.create_dataframe(dict={'name': ['OPTIMUS', 'BUMBLEBEE', 'EJECT'], 'age (M)': [5, 5, 5]})
-        self.assertTrue(result.equals(expected))
+        expected = self.create_dataframe(dict={('name', 'string'): ['OPTIMUS', 'BUMBLEBEE', 'EJECT'], ('age (M)', 'int64'): [5, 5, 5]}, force_data_types=True)
+        self.assertTrue(result.equals(expected, decimal=True))
 
 class TestExampleDask(TestExamplePandas):
     config = {'engine': 'dask', 'n_partitions': 1}
