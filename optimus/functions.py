6286603a986c6490256d14f1d0829075fd989226
Luis <luisaguirre027@gmail.com>
2021-07-29 19:20:31 -0400

    Fix functions from F bug

--- optimus/functions.py
+++ optimus/functions.py
@@ -5,7 +5,7 @@ class Functions(type):
     """
     Handle the method name and execute the function corresponding to the dataframe class
     """
-    def __getattr__(self, func):
+    def __getattr__(self, func) -> callable:
         """
 
         :param func: Method name
@@ -14,13 +14,18 @@ class Functions(type):
 
         def _func(df_or_value, *args):
             # Get the internal series in the optimus dataframe
-            # print("df_or_value",type(df_or_value),df_or_value)
             if isinstance(df_or_value, BaseDataFrame):
                 _df = df_or_value.data[df_or_value.cols.names(0)[0]]
                 # Convert the frame to a dataframe and to a optimus Dataframe
-                return df_or_value.__class__(getattr(df_or_value.functions, func)(_df, *args).to_frame())
+                series = getattr(df_or_value.functions, func)(_df, *args)
+                series.name = df_or_value.cols.names(0)[0]
+                r = df_or_value.__class__(series.to_frame())
             else:
-                return getattr(np, func)(df_or_value, *args)
+                r = getattr(np, func)(df_or_value, *args)
+
+            if isinstance(r, np.generic):
+                return np.asscalar(r)
+            return r
 
         return _func
 
