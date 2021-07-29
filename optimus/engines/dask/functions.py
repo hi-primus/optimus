6faedc7b037b6916bdb4a758ed7490b5c8426b4f
Luis <luisaguirre027@gmail.com>
2021-07-29 19:29:45 -0400

    Adjust dask functions to work properly with F object

--- optimus/engines/dask/functions.py
+++ optimus/engines/dask/functions.py
@@ -5,6 +5,7 @@
 from datetime import datetime
 
 import dask
+import dask.dataframe as dd
 import dask.array as da
 import pandas as pd
 from dask.array import stats
@@ -37,13 +38,13 @@ class DaskFunctions(DaskBaseFunctions, PandasBaseFunctions, Functions):
         return stats.skew(self.to_float(series))
 
     def exp(self, series):
-        return da.exp(self.to_float(series))
+        return dd.from_pandas(da.exp(self.to_float(series)), series.npartitions, None)
 
     def sqrt(self, series):
-        return da.sqrt(self.to_float(series))
+        return dd.from_pandas(da.sqrt(self.to_float(series)), series.npartitions, None)
 
     def reciprocal(self, series):
-        return da.reciprocal(self.to_float(series))
+        return dd.from_pandas(da.reciprocal(self.to_float(series)), series.npartitions, None)
 
     def unique_values(self, series, *args):
         # print("args",args)
@@ -51,66 +52,67 @@ class DaskFunctions(DaskBaseFunctions, PandasBaseFunctions, Functions):
         return self.to_string(series).unique()
 
     def radians(self, series):
-        return da.radians(self.to_float(series))
+        return dd.from_pandas(da.radians(self.to_float(series)), series.npartitions, None)
 
     def degrees(self, series):
-        return da.degrees(self.to_float(series))
+        return dd.from_pandas(da.degrees(self.to_float(series)), series.npartitions, None)
 
     def ln(self, series):
-        return da.log(self.to_float(series))
+        return dd.from_pandas(da.log(self.to_float(series)), series.npartitions, None)
 
     def log(self, series, base=10):
-        return da.log(self.to_float(series)) / da.log(base)
+        return dd.from_pandas(da.log(self.to_float(series)) / da.log(base), series.npartitions, None)
 
     def ceil(self, series):
-        return da.ceil(self.to_float(series))
+        return dd.from_pandas(da.ceil(self.to_float(series)), series.npartitions, None)
 
     def floor(self, series):
-        return da.floor(self.to_float(series))
+        return dd.from_pandas(da.floor(self.to_float(series)), series.npartitions, None)
 
     def sin(self, series):
-        return da.sin(self.to_float(series))
+        return dd.from_pandas(da.sin(self.to_float(series)), series.npartitions, None)
 
     def cos(self, series):
-        return da.cos(self.to_float(series))
+        return dd.from_pandas(da.cos(self.to_float(series)), series.npartitions, None)
 
     def tan(self, series):
-        return da.tan(self.to_float(series))
+        return dd.from_pandas(da.tan(self.to_float(series)), series.npartitions, None)
 
     def asin(self, series):
-        return da.arcsin(self.to_float(series))
+        return dd.from_pandas(da.arcsin(self.to_float(series)), series.npartitions, None)
 
     def acos(self, series):
-        return da.arccos(self.to_float(series))
+        return dd.from_pandas(da.arccos(self.to_float(series)), series.npartitions, None)
 
     def atan(self, series):
-        return da.arctan(self.to_float(series))
+        return dd.from_pandas(da.arctan(self.to_float(series)), series.npartitions, None)
 
     def sinh(self, series):
-        return da.arcsinh(self.to_float(series))
+        return dd.from_pandas(da.arcsinh(self.to_float(series)), series.npartitions, None)
 
     def cosh(self, series):
-        return da.cosh(self.to_float(series))
+        return dd.from_pandas(da.cosh(self.to_float(series)), series.npartitions, None)
 
     def tanh(self, series):
-        return da.tanh(self.to_float(series))
+        return dd.from_pandas(da.tanh(self.to_float(series)), series.npartitions, None)
 
     def asinh(self, series):
-        return da.arcsinh(self.to_float(series))
+        return dd.from_pandas(da.arcsinh(self.to_float(series)), series.npartitions, None)
 
     def acosh(self, series):
-        return da.arccosh(self.to_float(series))
+        return dd.from_pandas(da.arccosh(self.to_float(series)), series.npartitions, None)
 
     def atanh(self, series):
-        return da.arctanh(self.to_float(series))
+        return dd.from_pandas(da.arctanh(self.to_float(series)), series.npartitions, None)
 
     def normalize_chars(self, series):
         # str.decode return a float column. We are forcing to return a string again
         return series.str.normalize("NFKD").str.encode('ascii', errors='ignore').str.decode('utf8').astype(str)
 
     def format_date(self, series, current_format=None, output_format=None):
-        return pd.to_datetime(series, format=current_format, errors="coerce").dt.strftime(output_format)
+        return dd.from_pandas(pd.to_datetime(series, format=current_format,
+                              errors="coerce").dt.strftime(output_format), series.npartitions, None)
 
     def days_between(self, series, date_format=None):
-        return (pd.to_datetime(series, format=date_format,
-                               errors="coerce").dt.date - datetime.now().date())
+        return dd.from_pandas(pd.to_datetime(series, format=date_format,
+                              errors="coerce").dt.date - datetime.now().date(), series.npartitions, None)
