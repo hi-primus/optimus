37748b58840565af7b2a350b8947d533c9c4f3e7
Luis <luisaguirre027@gmail.com>
2021-08-11 13:31:43 -0400

    Remove unused constants

--- optimus/engines/base/dataframe/constants.py
+++ optimus/engines/base/dataframe/constants.py
@@ -3,4 +3,3 @@ from optimus.engines.base.dataframe.constants import BaseConstants
 class DataFrameBaseConstants(BaseConstants):
     pass
 
-STARTING_DASK = "Starting or setting Dask Client..."
--- optimus/engines/base/distributed/constants.py
+++ optimus/engines/base/distributed/constants.py
@@ -3,4 +3,3 @@ from optimus.engines.base.dataframe.constants import BaseConstants
 class DistributedBaseConstants(BaseConstants):
     pass
 
-STARTING_DASK = "Starting or setting Dask Client..."
