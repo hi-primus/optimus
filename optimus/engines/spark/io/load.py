d30509bd52a42cd0340ff437618504b49b1d2710
Luis <luisaguirre027@gmail.com>
2021-09-01 17:11:56 -0400

    Fix csv load bug on Spark

--- optimus/engines/spark/io/load.py
+++ optimus/engines/spark/io/load.py
@@ -21,6 +21,10 @@ from optimus.infer import is_list, is_url
 class Load(BaseLoad):
 
     @staticmethod
+    def df(*args, **kwargs):
+        return SparkDataFrame(*args, **kwargs)
+
+    @staticmethod
     def xml(path, *args, **kwargs) -> 'DataFrameType':
         pass
 
@@ -52,58 +56,24 @@ class Load(BaseLoad):
         df.meta = Meta.set(df.meta, value=df.meta.add_action("columns", df.cols.names()).get())
         return df
 
-    def csv(self, filepath_or_buffer, sep=",", header=True, infer_schema=True, encoding="UTF-8", n_rows=None,
-            null_value="None", quoting=3, lineterminator='\r\n', error_bad_lines=False, cache=False, na_filter=False,
-            storage_options=None, conn=None, n_partitions=1, *args, **kwargs):
+    @staticmethod
+    def _csv(filepath_or_buffer, *args, **kwargs):
 
-        if not is_url(filepath_or_buffer):
-            filepath_or_buffer = glob.glob(unquote_path(filepath_or_buffer))
-            meta = {"file_name": filepath_or_buffer, "name": ntpath.basename(filepath_or_buffer[0])}
-        else:
-            meta = {"file_name": filepath_or_buffer, "name": ntpath.basename(filepath_or_buffer)}
+        # TODO support arguments
+        
+        kwargs.pop("n_partitions", None)
+        kwargs.pop("encoding", None)
+        kwargs.pop("quoting", None)
+        kwargs.pop("on_bad_lines", None)
+        kwargs.pop("na_filter", None)
+        kwargs.pop("na_values", None)
+        kwargs.pop("storage_options", None)
 
-        try:
+        kwargs["index_col"] = kwargs.get("index_col", None)
+        if kwargs["index_col"] is False:
+            kwargs["index_col"] = None
 
-            # Pandas do not support \r\n terminator.
-            if lineterminator and lineterminator.encode(encoding='UTF-8', errors='strict') == b'\r\n':
-                lineterminator = None
-
-            if conn is not None:
-                filepath_or_buffer = conn.path(filepath_or_buffer)
-                storage_options = conn.storage_options
-            else:
-                storage_options = None
-
-            if kwargs.get("chunk_size") == "auto":
-                ## Chunk size is going to be 75% of the memory available
-                kwargs.pop("chunk_size")
-                kwargs["chunksize"] = psutil.virtual_memory().free * 0.75
-
-            na_filter = na_filter if null_value else False
-
-            def _read(_filepath_or_buffer):
-                return ks.read_csv(_filepath_or_buffer)
-
-            print(filepath_or_buffer[0])
-            if len(filepath_or_buffer) > 1:
-                df = ks.DataFrame()
-                for f in filepath_or_buffer:
-                    df = df.append(_read(f))
-            else:
-                df = _read(filepath_or_buffer[0])
-
-            # if isinstance(df, ks.io.parsers.TextFileReader):
-            #     df = df.get_chunk()
-
-            df = SparkDataFrame(df, op=self.op)
-
-            df.meta = Meta.set(df.meta, value=meta)
-
-        except IOError as error:
-            print(error)
-            logger.print(error)
-            raise
-
+        df = ks.read_csv(filepath_or_buffer, *args, **kwargs)
         return df
 
     @staticmethod
