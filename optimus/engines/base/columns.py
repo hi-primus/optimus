b49bc5108483a68ae7fedfe9a84c12a99ba4f3ee
Luis Aguirre <luisaguirre027@gmail.com>
2022-01-10 16:37:44 -0400

    Use full replace by default when passing a list of tuples

--- optimus/engines/base/columns.py
+++ optimus/engines/base/columns.py
@@ -2728,9 +2728,10 @@ class BaseColumns(ABC):
                 df = df.cols._replace(col, _search, _replace_by, search_by=search_by)
 
         else:
-            search_by = search_by or "chars"
             if is_list_of_tuples(search) and replace_by is None:
                 search, replace_by = zip(*search)
+                search_by = search_by or "full"
+            search_by = search_by or "chars"
             search = val_to_list(search, convert_tuple=True)
             replace_by = val_to_list(replace_by, convert_tuple=True)
             df = df.cols._replace(cols, search, replace_by, search_by, ignore_case, output_cols)
