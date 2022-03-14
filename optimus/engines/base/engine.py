3d3c008ece697f3e922694e0155d9e7de9027720
Argenis Leon <argenisleon@gmail.com>
2022-03-13 22:27:06 -0600

    Fix database base database connection

--- optimus/engines/base/engine.py
+++ optimus/engines/base/engine.py
@@ -5,6 +5,7 @@ import numpy as np
 from optimus.engines.base.constants import BaseConstants
 from optimus.engines.base.functions import BaseFunctions
 from optimus.engines.base.io.connect import Connect
+from optimus.engines.dask.io.jdbc import JDBC
 from optimus.helpers.logger import logger
 
 
@@ -20,13 +21,17 @@ class BaseEngine:
 
         logger.active(verbose)
 
-    @property
-    def connect(self):
+    @staticmethod
+    def connect(driver=None, host=None, database=None, user=None, password=None, port=None, schema="public",
+                oracle_tns=None, oracle_service_name=None, oracle_sid=None, presto_catalog=None,
+                cassandra_keyspace=None, cassandra_table=None, bigquery_project=None, bigquery_dataset=None):
         """
         Create the JDBC string connection
         :return: JDBC object
         """
-        return Connect(self)
+
+        return JDBC(host, database, user, password, port, driver, schema, oracle_tns, oracle_service_name, oracle_sid,
+                    presto_catalog, cassandra_keyspace, cassandra_table, bigquery_project, bigquery_dataset)
 
     @property
     def nan(self):
--- optimus/engines/ibis/engine.py
+++ optimus/engines/ibis/engine.py
@@ -1,19 +1,18 @@
 import ibis
 
+from optimus._version import __version__
 from optimus.engines.base.engine import BaseEngine
 from optimus.engines.ibis.io.jdbc import JDBC
 from optimus.optimus import Engine, EnginePretty
-from optimus._version import __version__
 
 
 class IbisEngine(BaseEngine):
     __version__ = __version__
 
-    def __init__(self, verbose=False, comm=None, *args, **kwargs):
+    def __init__(self, verbose=False, op=None, comm=None, *args, **kwargs):
         self.engine = Engine.IBIS.value
 
         self.verbose(verbose)
-        self.op = op
         self.client = ibis
 
     @property
@@ -21,8 +20,7 @@ class IbisEngine(BaseEngine):
         from optimus.engines.ibis.functions import IbisFunctions
         return IbisFunctions(self)
 
-    @staticmethod
-    def connect(driver=None, host=None, database=None, user=None, password=None, port=None, schema="public",
+    def connect(self, driver=None, host=None, database=None, user=None, password=None, port=None, schema="public",
                 oracle_tns=None, oracle_service_name=None, oracle_sid=None, presto_catalog=None,
                 cassandra_keyspace=None, cassandra_table=None, bigquery_project=None, bigquery_dataset=None):
         """
@@ -31,7 +29,7 @@ class IbisEngine(BaseEngine):
         """
 
         return JDBC(host, database, user, password, port, driver, schema, oracle_tns, oracle_service_name, oracle_sid,
-                    presto_catalog, cassandra_keyspace, cassandra_table, bigquery_project, bigquery_dataset)
+                    presto_catalog, cassandra_keyspace, cassandra_table, bigquery_project, bigquery_dataset, op=self)
 
     @property
     def engine_label(self):
