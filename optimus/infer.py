1d0a81b79799d556446e0200bdb463f653d0a2af
Argenis Leon <argenisleon@gmail.com>
2022-11-07 13:36:08 -0600

    Fix is_url when not a string

--- optimus/infer.py
+++ optimus/infer.py
@@ -811,15 +811,18 @@ def is_float_like(value):
 
 
 def is_url(value):
-    regex = re.compile(
-        r'^(?:http|ftp|hdfs)s?://'  # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-        r'localhost|'  # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        r'(?::\d+)?'  # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-    return re.match(regex, value)
+    if is_str(value):
+        regex = re.compile(
+            r'^(?:http|ftp|hdfs)s?://'  # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+            r'localhost|'  # localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        result = re.match(regex, value)
+    else:
+        result = False
+    return result
 
 
 def is_float(value):
